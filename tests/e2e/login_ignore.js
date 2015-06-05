35326fee71bde2a61259f85031beb72089d8a02c
Ken Andrew <noguchi.andrew@gmail.com>
2015-06-05 18:09:37 -0400

    Removing file.

--- tests/e2e/login_ignore.js
+++
@@ -1,6 +0,0 @@
-// This file should be ignored as it contains login credentials
-
-exports.uid = 'f873682e-177a-443e-9c1f-ec05d6134d14';
-exports.pw = '3J#xPukqeHzd#5xE';
-exports.pwNew = '2$E6SCAFZ!yPniz$';
-exports.betaKey = '5f871452';
