5f10be6e3dedb283e2732debbbb7d54fe7e35d1c
Karina <karina@blockchain.com>
2015-09-09 16:16:37 -0400

    chore(es6): converted ConfirmRecoverPhraseCtrl

---
+++ assets/js/controllers/confirm_recovery_phrase_ctrl.js
@@ -0,0 +1,95 @@
+walletApp.controller("ConfirmRecoveryPhraseCtrl", ($scope, $log, Wallet, $modalInstance, $translate) => {
+  $scope.step = 1;
+  $scope.recoveryPhrase = null;
+  $scope.words = [
+    {
+      value: '',
+      actual: '',
+      index: null,
+      error: null
+    }, {
+      value: '',
+      actual: '',
+      index: null,
+      error: null
+    }, {
+      value: '',
+      actual: '',
+      index: null,
+      error: null
+    }, {
+      value: '',
+      actual: '',
+      index: null,
+      error: null
+    }
+  ];
+
+  $scope.setRandomWords = recoveryPhrase => {
+    let word;
+    let currentWordIndex = 0;
+    let results = [];
+    while (currentWordIndex < 4) {
+      const randIndex = $scope.getRandInRange(0, 11);
+      const randWord = recoveryPhrase.split(' ').splice(randIndex, 1).toString();
+      if ($scope.hasWordBeenUsed(randIndex + 1)) {
+        continue;
+      }
+      word = $scope.words[currentWordIndex++];
+      word.actual = randWord;
+      results.push(word.index = randIndex + 1);
+    }
+    results;
+  };
+
+  $scope.hasWordBeenUsed = index => {
+    return ($scope.words.some(e => e.index === index));
+  }
+
+
+  $scope.getRandInRange = (min, max) => {
+    Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  $scope.close = () => {
+    $modalInstance.dismiss("");
+  };
+
+  $scope.goToVerify = () => {
+    $scope.step = 2;
+  };
+
+  $scope.verify = () => {
+    let valid = true;
+    let ref = $scope.words;
+
+    for (let i = 0, len = ref.length; i < len; i++) {
+      let word = ref[i];
+      if (word.error = word.value.toLowerCase() !== word.actual) {
+        valid = false;
+      }
+    }
+    if (valid) {
+      Wallet.confirmRecoveryPhrase();
+      $scope.step = 3;
+    }
+  };
+
+  const success = mnemonic => {
+    $scope.recoveryPhrase = mnemonic;
+    $scope.setRandomWords(mnemonic);
+  };
+
+  const error = error => {
+    $translate(error).then( translation => {
+      Wallet.displayError(translation);
+    });
+    $modalInstance.dismiss("");
+  };
+
+  const cancel = () => {
+    $modalInstance.dismiss("");
+  };
+
+  Wallet.getMnemonic(success, error, cancel);
+});
