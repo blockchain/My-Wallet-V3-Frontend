ce67bcdf65c7478406d9fc6c511181af10caab66
Karina <karina@blockchain.com>
2015-09-10 00:30:45 -0400

    chore(es6): converted OpenLinkCtrl

---
+++ assets/js/controllers/open_link_ctrl.js
@@ -0,0 +1,10 @@
+walletApp.controller("OpenLinkController", ($scope, Wallet, $translate, $stateParams, $state) => {
+  const paymentRequest = Wallet.parsePaymentRequest($stateParams.uri);
+  Wallet.goal.send = paymentRequest;
+  if (!Wallet.status.isLoggedIn) {
+    $translate("PLEASE_LOGIN_FIRST").then(translation => {
+      Wallet.displayInfo(translation, true);
+    });
+    $state.go("login.show");
+  }
+});
