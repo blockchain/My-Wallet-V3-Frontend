8df1e934f30e8b93acc19936ee6978ac66cdd6fd
Abhi Sharma <abhisharma2@gmail.com>
2015-09-15 15:59:10 -0400

    feat(Home): add tabular account balance data, rm related tests

--- assets/js/controllers/home_ctrl.js
+++ assets/js/controllers/home_ctrl.js
@@ -1,24 +1,14 @@
 walletApp.controller("HomeCtrl", ($scope, $window, Wallet, $modal) => {
-  $scope.accounts = Wallet.accounts;
   $scope.status = Wallet.status;
   $scope.settings = Wallet.settings;
   $scope.getTotal = () => Wallet.total('accounts');
-  $scope.empty = false;
+  $scope.legacyTotal = () => Wallet.getTotalBalanceForActiveLegacyAddresses();
   $scope.transactions = [];
-
-  $scope.accountFilter = account => {
-    return account.balance > 0 && !account.archived;
-  };
+  $scope.activeAccounts = Wallet.accounts().filter(a => { return !(a.archived); })
 
   $scope.$watch('status.didLoadTransactions', didLoad => {
     if (!didLoad) return;
     $scope.transactions = Wallet.transactions;
-  });
-
-
-  $scope.$watchCollection('accounts()', accounts => {
-    if (accounts.length === 0 || !$scope.status.didLoadBalances) return;
-    console.log('hello')
   });
 
   if ($scope.status.firstTime) {
