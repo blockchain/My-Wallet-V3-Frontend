52a98609175348dd682172c0c13f43687229d891
Justin Tormey <jrtormey@gmail.com>
2015-10-22 01:11:07 +0100

    feat(send): fancy new input for selecting destination addresses and accounts

---
+++ assets/js/directives/destinationInput.directive.js
@@ -0,0 +1,56 @@
+angular
+  .module('walletApp')
+  .directive('destinationInput', destinationInput);
+
+destinationInput.$inject = ['$rootScope', '$timeout', 'Wallet'];
+
+function destinationInput($rootScope, $timeout, Wallet) {
+  const directive = {
+    restrict: 'E',
+    require: 'ngModel',
+    scope: {
+      model: '=ngModel',
+      onRequestQr: '&',
+      change: '&ngChange'
+    },
+    templateUrl: 'templates/destination-input.jade',
+    link: link
+  };
+  return directive;
+
+  function link(scope, elem, attrs, ctrl) {
+    scope.browserWithCamera = $rootScope.browserWithCamera;
+    scope.accounts = Wallet.accounts().filter(a => a.active);
+
+    let format = (a, type) => ({
+      label     : a.label,
+      balance   : a.balance,
+      active    : a.active,
+      archived  : a.archived,
+      type      : type
+    });
+
+    scope.setModel = (a) => {
+      scope.model = format(a, 'Accounts');
+      scope.model.index = a.index;
+      $timeout(scope.change);
+    };
+
+    scope.clearModel = () => {
+      scope.model = format({}, 'External');
+      scope.model.address = '';
+      scope.focusInput();
+      $timeout(scope.change);
+    };
+
+    scope.focusInput = () => {
+      $timeout(() => elem.find('input')[0].focus(), 50);
+    };
+
+    if (!scope.model) {
+      scope.clearModel();
+    }
+
+    scope.$watch('model', scope.change);
+  }
+}
