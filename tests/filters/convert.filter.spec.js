e264a6d896eb2aa43d5abbc2574bd5cfec884cd9
Justin Tormey <jrtormey@gmail.com>
2015-12-08 10:30:41 -0500

    test(filter): add test spec for convert filter

---
+++ tests/filters/convert.filter.spec.js
@@ -0,0 +1,27 @@
+describe('convertFilter', () => {
+
+  beforeEach(module('walletApp'));
+  beforeEach(module('walletFilters'));
+
+  beforeEach(inject((Wallet, currency) => {
+    Wallet.settings.displayCurrency = currency.bitCurrencies[0];
+  }));
+
+  it('should convert from satoshi to the displayCurrency', inject(($filter) => {
+    let convert = $filter('convert');
+    expect(convert(100000000)).toBe('1 BTC');
+  }));
+
+  it('should change when the displayCurrency changes', inject(($filter, Wallet, currency) => {
+    let convert = $filter('convert');
+    expect(convert(100000000)).toBe('1 BTC');
+    Wallet.settings.displayCurrency = currency.bitCurrencies[1];
+    expect(convert(100000000)).toBe('1000 mBTC');
+  }));
+
+  it('should not convert if the amount is not defined', inject(($filter) => {
+    let convert = $filter('convert');
+    expect(convert()).toBe(null);
+  }));
+
+});
