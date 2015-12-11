8041a48c6265a39c96f76685bace292043890554
Justin Tormey <jrtormey@gmail.com>
2015-12-11 15:22:51 -0500

    refactor(FiatOrBtc): coffee to es6

---
+++ assets/js/directives/fiat-or-btc.directive.js
@@ -0,0 +1,28 @@
+
+angular
+  .module('walletApp')
+  .directive('fiatOrBtc', fiatOrBtc);
+
+fiatOrBtc.$inject = ['Wallet', 'currency'];
+
+function fiatOrBtc(Wallet, currency) {
+  const directive = {
+    restrict: 'E',
+    replace: false,
+    scope: {
+      btc: '='
+    },
+    templateUrl: 'templates/fiat-or-btc.jade',
+    link: link
+  };
+  return directive;
+
+  function link(scope, elem, attrs) {
+    scope.settings = Wallet.settings;
+    scope.currency = scope.settings.displayCurrency;
+    scope.isBitCurrency = currency.isBitCurrency;
+
+    scope.updateDisplay = () => scope.currency = Wallet.settings.displayCurrency;
+    scope.$watch('settings.displayCurrency', scope.updateDisplay);
+  }
+}
