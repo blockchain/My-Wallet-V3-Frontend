69e477cf59c683f0b74c453d682f2aa2427704f8
Sjors Provoost <sjors@sprovoost.nl>
2015-12-11 15:26:13 +0100

    refactor(Cleanup): remove orphaned code country-data and browserify

--- assets/js/country-data-import.js
+++
@@ -1,4 +0,0 @@
-// Combined with "browserify -r country-data ..." in package.json this gives 
-// us a global "require('country-data');"
-// There might be a better way to achieve this...
-require('country-data')
