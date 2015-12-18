ae45e53eae09c4b9a305ebe72074aac0f39e4433
Justin Tormey <jrtormey@gmail.com>
2015-12-18 11:11:52 -0500

    refactor(DeCoffee): convert is-not-equal directive to es6

---
+++ assets/js/directives/is-not-equal.directive.js
@@ -0,0 +1,21 @@
+
+angular
+  .module('walletApp')
+  .directive('isNotEqual', isNotEqual);
+
+isNotEqual.$inject = ['Wallet'];
+
+function isNotEqual(Wallet) {
+  const directive = {
+    restrict: 'A',
+    require: 'ngModel',
+    link: link
+  };
+  return directive;
+
+  function link(scope, elem, attrs, ctrl) {
+    ctrl.$validators.isNotEqual = (modelValue, viewValue) => {
+      return attrs.isNotEqual !== viewValue;
+    };
+  }
+}
