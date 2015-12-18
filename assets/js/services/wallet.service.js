9ea51ccb2c74d57bc658f338c62a24ef93eb45a3
Sjors Provoost <sjors@sprovoost.nl>
2015-12-18 16:05:14 +0100

    feat(Unsubscribe): unsubscribe route

--- assets/js/services/wallet.service.js
+++ assets/js/services/wallet.service.js
@@ -1219,6 +1219,20 @@ function Wallet($http, $window, $timeout, Alerts, MyWallet, MyBlockchainApi, MyB
     wallet.tokenEndpoints.verifyEmail(token, success, error);
   }
 
+  wallet.unsubscribe = (token, successCallback, errorCallback) => {
+    const success = (guid) => {
+      successCallback(guid);
+      wallet.applyIfNeeded();
+    }
+
+    const error = (message) => {
+      console.log(message);
+      errorCallback(message);
+    }
+
+    wallet.tokenEndpoints.unsubscribe(token, success, error);
+  }
+
   // Testing: only works on mock MyWallet
 
   wallet.refresh = () => {
