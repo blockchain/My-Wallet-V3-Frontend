c5ad6579fcb8e68f6ce6720e6823f6b4ed33066e
Sjors Provoost <sjors@sprovoost.nl>
2016-01-19 17:38:35 +0000

    Dev(Server): allow connecting to api.blockchain.info

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
       "style-src 'self' 'sha256-vv5i1tRAGZ/gOQeRpI3CEWtvnCpu5FCixlD2ZPu7h84=' 'sha256-47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU='",
       "child-src 'none'",
       "script-src 'self' 'sha256-mBeSvdVuQxRa2pGoL8lzKX14b2vKgssqQoW36iRlU9g=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
-      "connect-src 'self' " + rootURL + " " + (webSocketURL || "wss://*.blockchain.info"),
+      "connect-src 'self' " + rootURL + " " + (webSocketURL || "wss://*.blockchain.info") + " https://api.blockchain.info",
       "object-src 'none'",
       "media-src 'self' data: mediastream: blob:",
       "font-src 'self'", ''
