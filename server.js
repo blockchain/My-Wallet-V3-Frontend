d268a1f0b6a0b9c853439b597410d445662bef68
Justin Tormey <jrtormey@gmail.com>
2016-02-01 12:53:23 -0500

    fix(DevServer): use correct express api to stop deprecation warning

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ if (dist) {
 }
 
 app.use(function (req, res) {
-  res.send(404,'<center><h1>404 Not Found</h1></center>');
+  res.status(404).send('<center><h1>404 Not Found</h1></center>');
 });
 
 app.listen(port, function () {
