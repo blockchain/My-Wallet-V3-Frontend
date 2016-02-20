177ba6271567a35c58d6ac3b8959bf3c24822533
Justin Tormey <jrtormey@gmail.com>
2016-02-20 11:13:51 -0500

    fix(State): prevent navigation to marketing page while logged in

--- assets/js/controllers/app.controller.js
+++ assets/js/controllers/app.controller.js
@@ -61,6 +61,12 @@ function AppCtrl($scope, Wallet, Alerts, $state, $rootScope, $cookies, $location
     }
   };
 
+  $scope.$on('$stateChangeStart', (event, toState, toParams, fromState, fromParams) => {
+    if (toState.name === 'welcome' && Wallet.status.isLoggedIn) {
+      event.preventDefault();
+    }
+  });
+
   $scope.$on('$stateChangeSuccess', (event, toState, toParams, fromState, fromParams) => {
     let loggedOutStates = ['public', "welcome", 'public.login-no-uid', 'public.login-uid', 'public.reset-two-factor', 'public.recover', 'public.reminder', 'public.signup', 'public.help', 'open', 'wallet.common.verify-email', 'wallet.common.unsubscribe', 'public.authorize-approve', 'public.reset-two-factor-token'];
     if (loggedOutStates.every(s => toState.name !== s) && $scope.status.isLoggedIn === false) {
