b11c8992b7aa6e6b465e1e6455357ca30a5f3c9e
Justin Tormey <jrtormey@gmail.com>
2016-03-02 13:11:18 -0500

    refactor(DeCoffee): convert tor directive to es6

---
+++ assets/js/directives/tor.directive.js
@@ -0,0 +1,24 @@
+
+angular
+  .module('walletApp')
+  .directive('tor', tor);
+
+function tor($translate, Wallet) {
+  const directive = {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      _buttonClass: '@buttonClass'
+    },
+    templateUrl: 'templates/tor.jade',
+    link: link
+  };
+  return directive;
+
+  function link(scope, elem, attrs) {
+    scope.buttonClass = scope._buttonClass || 'button-primary';
+    scope.settings = Wallet.settings;
+    scope.enableBlockTOR = () => Wallet.enableBlockTOR();
+    scope.disableBlockTOR = () => Wallet.disableBlockTOR();
+  }
+}
