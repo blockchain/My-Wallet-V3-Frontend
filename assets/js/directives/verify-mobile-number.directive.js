c2bc39958427eda96f19a5209b2cf96d5eb179c9
Justin Tormey <jrtormey@gmail.com>
2016-03-02 14:40:07 -0500

    refactor(DeCoffee): convert verify-mobile-number directive to es6

---
+++ assets/js/directives/verify-mobile-number.directive.js
@@ -0,0 +1,68 @@
+
+angular
+  .module('walletApp')
+  .directive('verifyMobileNumber', verifyMobileNumber);
+
+function verifyMobileNumber($translate, Wallet, $filter) {
+  const directive = {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      onSuccess: '&'
+    },
+    templateUrl: 'templates/verify-mobile-number.jade',
+    link: link
+  };
+  return directive;
+
+  function link(scope, elem, attrs) {
+    if (attrs.buttonLg != null) scope.buttonLg = true;
+    if (attrs.fullWidth != null) scope.fullWidth = true;
+
+    scope.status = {
+      busy: false,
+      retrying: false,
+      retrySuccess: false
+    };
+    scope.errors = {
+      verify: null,
+      retryFail: null
+    };
+
+    scope.retrySendCode = () => {
+      scope.status.retrying = true;
+
+      let success = () => {
+        scope.status.retrying = false;
+        scope.status.retrySuccess = true;
+        scope.errors.retryFail = null;
+      };
+
+      let error = () => {
+        scope.status.retrying = false;
+        scope.status.retrySuccess = false;
+        scope.errors.retryFail = 'Error resending verification code';
+      };
+
+      Wallet.changeMobile(Wallet.user.mobile, success, error);
+    };
+
+    scope.verifyMobile = (code) => {
+      scope.status.busy = true;
+
+      let success = () => {
+        scope.code = '';
+        scope.errors.verify = '';
+        scope.status.busy = false;
+        scope.onSuccess();
+      };
+
+      let error = (message) => {
+        scope.errors.verify = message;
+        scope.status.busy = false;
+      };
+
+      Wallet.verifyMobile(code, success, error);
+    };
+  }
+}
