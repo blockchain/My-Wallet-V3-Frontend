08de35afb48b3e557303e8eab72b8816afc1ca23
plondon <plondon@gm.slc.edu>
2016-03-11 11:02:19 +0000

    feat(LoadingState): add rocket loading state

---
+++ assets/js/directives/blocket-loading.directive.js
@@ -0,0 +1,34 @@
+
+angular
+  .module('walletApp')
+  .directive('blocketLoading', blocketLoading);
+
+blocketLoading.$inject = ['$rootScope', '$timeout'];
+
+function blocketLoading($rootScope, $timeout) {
+  const directive = {
+    restrict: 'E',
+    templateUrl: 'templates/blocket-loading.jade',
+    link: link
+  }
+
+  return directive;
+
+  function link(scope, elem, attrs) {
+    // give it a sec
+    scope.loading = true;
+    $timeout(() => { scope.waiting = true; }, 500)
+
+    // doc is ready, time for liftoff
+    scope.docIsReady = () => {
+      // if wait was not long enough just hide
+      if (!scope.waiting) {scope.waiting = true; return;}
+
+      scope.liftoff = true
+      $timeout(() => { scope.orbit = true }, 1000)
+      $timeout(() => { scope.loading = false }, 1000)
+    };
+
+    angular.element(document).ready(scope.docIsReady)
+  }
+}
