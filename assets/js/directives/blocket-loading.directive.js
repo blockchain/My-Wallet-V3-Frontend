b3294135befb6f9514c017b9bb6159c420a82c5c
plondon <plondon@gm.slc.edu>
2016-03-11 20:54:43 +0000

    refactor(LoadingState): refactor directive and change variable names for clarity

--- assets/js/directives/blocket-loading.directive.js
+++ assets/js/directives/blocket-loading.directive.js
@@ -8,6 +8,7 @@ blocketLoading.$inject = ['$rootScope', '$timeout'];
 function blocketLoading($rootScope, $timeout) {
   const directive = {
     restrict: 'E',
+    scope: {},
     templateUrl: 'templates/blocket-loading.jade',
     link: link
   }
@@ -15,20 +16,17 @@ function blocketLoading($rootScope, $timeout) {
   return directive;
 
   function link(scope, elem, attrs) {
-    // give it a sec
     $timeout(() => {
-      scope.loading = true; 
-      scope.waiting = true;
+      scope.timeout = true;
+      scope.windowLoading = true; 
     }, 500)
 
-    // doc is ready, time for liftoff
     scope.docIsReady = () => {
-      // if wait was not long enough just hide
-      if (!scope.waiting) {scope.hide = true; return;}
+      if (!scope.timeout) { scope.hide=true; return; }
 
-      scope.liftoff = true
-      $timeout(() => { scope.orbit = true }, 1000)
-      $timeout(() => { scope.loading = false }, 1000)
+      $timeout(() => { scope.liftoff = true }, 0)
+      $timeout(() => { scope.orbiting = true }, 1000)
+      $timeout(() => { scope.windowLoading = false }, 1000)
     };
 
     angular.element(document).ready(scope.docIsReady)
