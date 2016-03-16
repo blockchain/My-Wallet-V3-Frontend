908d752dcebb349684f10b60a040eb9b318b07c6
plondon <plondon@gm.slc.edu>
2016-03-16 07:55:16 -0400

    feat(BlocketLoading): integrate blocket-loading

--- assets/js/directives/blocket-loading.directive.js
+++ assets/js/directives/blocket-loading.directive.js
@@ -16,17 +16,10 @@ function blocketLoading($rootScope, $timeout) {
   return directive;
 
   function link(scope, elem, attrs) {
-    $timeout(() => {
-      scope.timeout = true;
-      scope.windowLoading = true; 
-    }, 500)
-
     scope.docIsReady = () => {
-      if (!scope.timeout) { scope.hide=true; return; }
-
       $timeout(() => { scope.liftoff = true }, 0)
       $timeout(() => { scope.orbiting = true }, 1000)
-      $timeout(() => { scope.windowLoading = false }, 1000)
+      $timeout(() => { elem.remove() }, 2000)
     };
 
     angular.element(document).ready(scope.docIsReady)
