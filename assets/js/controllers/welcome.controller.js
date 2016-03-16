c4bb24a4048cf47f0160967b3ea4410a0dbcb1af
plondon <plondon@gm.slc.edu>
2016-03-16 17:10:54 -0400

    feat(MarketingPage): add scroll functionality to marketing page

--- assets/js/controllers/welcome.controller.js
+++ assets/js/controllers/welcome.controller.js
@@ -2,6 +2,43 @@ angular
   .module('walletApp')
   .controller("WelcomeCtrl", WelcomeCtrl);
 
-function WelcomeCtrl($scope) {
-    
+function WelcomeCtrl($scope, $window, $timeout) {
+  let scrollTo = (element, to, duration) => {
+      let start = element.scrollTop,
+          change = to - start,
+          increment = 20;
+
+      let animateScroll = (elapsedTime) => {        
+          elapsedTime += increment;
+          let position = easeInOut(elapsedTime, start, change, duration);                        
+          element.scrollTop = position; 
+          if (elapsedTime < duration) {
+              $timeout(() => {
+                  animateScroll(elapsedTime);
+              }, increment);
+          }
+      };
+
+      animateScroll(0);
+  }
+
+  let easeInOut = (currentTime, start, change, duration) => {
+      currentTime /= duration / 2;
+      if (currentTime < 1) {
+          return change / 2 * currentTime * currentTime + start;
+      }
+      currentTime -= 1;
+      return -change / 2 * (currentTime * (currentTime - 2) - 1) + start;
+  }
+
+  $scope.scroll = (userType) => {
+    let top = 0;
+    if (userType === 'newUser') {
+      top = angular.element(newUser)[0].offsetTop;
+    } else {
+      top = angular.element(experiencedUser)[0].offsetTop;
+    }
+    scrollTo(document.body, top, 500)
+  }
+
 }
