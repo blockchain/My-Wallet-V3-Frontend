613153642375ec130753aefbedf8223502299141
Sjors Provoost <sjors@sprovoost.nl>
2016-04-25 13:18:45 +0200

    refactor(server.js): remove unused code and obey semistandard

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 'use strict';
 
-var express         = require('express')
-  , ejs             = require('ejs')
-  , path            = require('path')
+var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
 
 loadEnv('.env');
 
-var port      = process.env.PORT || 8080
-  , dist      = parseInt(process.env.DIST) === 1
-  , origins   = (process.env.BLOCKCHAIN || '').split(' ')
-  , whitelist = (process.env.IP_WHITELIST || '').split(' ')
-  , rootURL   = process.env.ROOT_URL || 'https://blockchain.info/'
-  , webSocketURL = process.env.WEBSOCKET_URL || false
-  , apiDomain = process.env.API_DOMAIN
+var port = process.env.PORT || 8080;
+var dist = parseInt(process.env.DIST, 10) === 1;
+var rootURL = process.env.ROOT_URL || 'https://blockchain.info/';
+var webSocketURL = process.env.WEBSOCKET_URL || false;
+var apiDomain = process.env.API_DOMAIN;
 
 // App configuration
 var rootApp = express();
 var app = express();
 
-rootApp.use("/wallet-beta", app);
+rootApp.use('/wallet-beta', app);
 
 app.use(function (req, res, next) {
   if (req.url === '/') {
     var cspHeader = ([
-      "img-src 'self' " + rootURL + " data:",
+      "img-src 'self' " + rootURL + ' data:',
       // echo -n "outline: 0;" | openssl dgst -sha256 -binary | base64
       // "outline: 0;"        : ud+9... from ui-select
       // "margin-right: 10px" : 4If ... from ui-select
@@ -33,7 +31,7 @@ app.use(function (req, res, next) {
       "style-src 'self' 'uD+9kGdg1SXQagzGsu2+gAKYXqLRT/E07bh4OhgXN8Y=' '4IfJmohiqxpxzt6KnJiLmxBD72c3jkRoQ+8K5HT5K8o='",
       "child-src 'none'",
       "script-src 'self' 'sha256-mBeSvdVuQxRa2pGoL8lzKX14b2vKgssqQoW36iRlU9g=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
-      "connect-src 'self' " + rootURL + " " + (webSocketURL || "wss://*.blockchain.info") + " " + (apiDomain ||  "https://api.blockchain.info"),
+      "connect-src 'self' " + rootURL + ' ' + (webSocketURL || 'wss://*.blockchain.info') + ' ' + (apiDomain || 'https://api.blockchain.info'),
       "object-src 'none'",
       "media-src 'self' https://storage.googleapis.com/bc_public_assets/ data: mediastream: blob:",
       "font-src 'self'", ''
@@ -41,17 +39,12 @@ app.use(function (req, res, next) {
     res.setHeader('content-security-policy', cspHeader);
     res.setHeader('X-Frame-Options', 'SAMEORIGIN');
 
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    if (whitelist != '' && whitelist.indexOf(ip.split(', ')[0]) < 0) {
-      console.log(ip);
-      res.status(403).send('I\'m sorry Dave, I can\'t let you do that.');
-    } else if (dist) {
+    if (dist) {
       res.render('index.html');
     } else {
       res.render('app/index.jade');
     }
     return;
-
   }
   if (dist) {
     res.setHeader('Cache-Control', 'public, max-age=31557600');
@@ -95,20 +88,8 @@ rootApp.listen(port, function () {
   console.log('Listening on %d', port);
 });
 
-// Custom middleware
-function allowOrigins(origins) {
-  return function (req, res, next) {
-    origins.forEach(function (origin) {
-      if (req.headers.origin != null && req.headers.origin.indexOf(origin) > -1) {
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-      }
-    });
-    next();
-  };
-}
-
 // Helper functions
-function loadEnv(envFile) {
+function loadEnv (envFile) {
   try {
     require('node-env-file')(envFile);
   } catch (e) {
