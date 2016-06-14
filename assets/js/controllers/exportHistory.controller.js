a16f52f2ae8df729417a83c843b47e7bcbc4f687
Justin Tormey <justin@blockchain.com>
2016-06-14 17:38:12 -0400

    feat(ExportHistory): add all imported and all hd addresses to select options

--- assets/js/controllers/exportHistory.controller.js
+++ assets/js/controllers/exportHistory.controller.js
@@ -2,10 +2,23 @@ angular
   .module('walletApp')
   .controller('ExportHistoryController', ExportHistoryController);
 
-function ExportHistoryController ($scope, $sce, format, Wallet, MyWallet, activeIndex) {
+function ExportHistoryController ($scope, $sce, $translate, format, Wallet, MyWallet, activeIndex) {
   let accounts = Wallet.accounts().filter(a => !a.archived && a.index != null);
   let addresses = Wallet.legacyAddresses().filter(a => !a.archived);
-  $scope.targets = accounts.concat(addresses).map(format.origin);
+
+  let allHD = {
+    type: $translate.instant('ALL'),
+    label: $translate.instant('HD_ADDRESSES'),
+    address: accounts.map(a => a.extendedPublicKey).join('|')
+  };
+
+  let allAddresses = {
+    type: $translate.instant('ALL'),
+    label: $translate.instant('IMPORTED_ADDRESSES'),
+    address: addresses.map(a => a.address).join('|')
+  };
+
+  $scope.targets = [allHD, allAddresses].concat(accounts.concat(addresses).concat(allAddresses).map(format.origin));
 
   $scope.activeCount = (
     Wallet.accounts().filter(a => !a.archived).length +
@@ -17,12 +30,22 @@ function ExportHistoryController ($scope, $sce, format, Wallet, MyWallet, active
     $scope.active = t.index != null ? t.xpub : t.address;
   };
 
-  for (let i = 0; i < $scope.targets.length; i++) {
-    if (i === parseInt(activeIndex, 10)) {
-      $scope.target = $scope.targets[i];
-      $scope.setActive();
-      break;
+  if (activeIndex === '') {
+    $scope.target = allHD;
+  } else if (activeIndex === 'imported') {
+    $scope.target = allAddresses;
+  } else if (!isNaN(activeIndex)) {
+    for (let i = 0; i < $scope.targets.length; i++) {
+      let target = $scope.targets[i];
+      if (target.index === parseInt(activeIndex, 10)) {
+        $scope.target = target;
+        break;
+      }
     }
+  }
+
+  if ($scope.target) {
+    $scope.setActive();
   }
 
   $scope.action = $sce.trustAsResourceUrl(`${$scope.rootURL}export-history`);
