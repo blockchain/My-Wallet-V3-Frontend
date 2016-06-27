ebffc3363fe1dc07525cd022a079beebdbac9076
Justin Tormey <justin@blockchain.com>
2016-06-27 14:47:03 -0400

    fix(ConfirmRecovery): always display as button-primary

--- assets/js/directives/confirm-recovery-phrase.directive.js
+++ assets/js/directives/confirm-recovery-phrase.directive.js
@@ -9,18 +9,13 @@ function confirmRecoveryPhrase ($uibModal, Wallet) {
   const directive = {
     restrict: 'E',
     replace: true,
-    scope: {
-      _buttonClass: '@buttonClass'
-    },
+    scope: {},
     templateUrl: 'templates/confirm-recovery-phrase.jade',
     link: link
   };
   return directive;
 
   function link (scope, elem, attrs) {
-    scope.status = Wallet.status;
-    scope.buttonClass = scope._buttonClass || 'button-primary';
-
     scope.confirmRecoveryPhrase = () => $uibModal.open({
       templateUrl: 'partials/confirm-recovery-phrase-modal.jade',
       controller: 'ConfirmRecoveryPhraseCtrl',
