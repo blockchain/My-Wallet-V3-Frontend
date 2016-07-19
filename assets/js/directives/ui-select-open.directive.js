6583d26629daea6120e7af9815a3b87d7bf510aa
Justin Tormey <justin@blockchain.com>
2016-07-19 14:11:24 -0400

    test(UiSelectOpen): add test spec for ui-select-open directive

--- assets/js/directives/ui-select-open.directive.js
+++ assets/js/directives/ui-select-open.directive.js
@@ -7,9 +7,12 @@ function uiSelectOpen ($timeout) {
     restrict: 'A',
     require: 'uiSelect',
     link: function (scope, element, attrs, uiSelect) {
+      // Allows programmatically opening/closing ui-select
       scope.$watch(
         () => scope.$eval(attrs.uiSelectOpen),
         (open) => $timeout(() => uiSelect.open = open, 50));
+
+      // Adds a uiSelectOnClose listener, there is no onClose in the uiSelect API
       scope.$watch(
         () => uiSelect.open,
         (open) => !open && $timeout(() => scope.$eval(attrs.uiSelectOnClose), 50));
