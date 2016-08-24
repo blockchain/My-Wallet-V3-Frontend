e9d96ffd050d2e22bd7d1c972141e01d21ce3ceb
Justin Tormey <justin@blockchain.com>
2016-08-24 14:53:04 -0400

    feat(KYC): display different kyc states on buy sell home page

--- assets/js/components/kyc-status.component.js
+++ assets/js/components/kyc-status.component.js
@@ -2,7 +2,16 @@ angular
   .module('walletApp')
   .component('kycStatus', {
     bindings: {
+      state: '=',
       onTrigger: '&'
     },
-    templateUrl: 'templates/kyc-status.jade'
+    templateUrl: 'templates/kyc-status.jade',
+    controller: function (buySell) {
+      this.stateMap = {
+        'pending': { ns: 'KYC_IN_REVIEW', i: 'ti-alert' },
+        'rejected': { ns: 'KYC_REJECTED', i: 'ti-close' },
+        'expired': { ns: 'KYC_EXPIRED', i: 'ti-timer' }
+      };
+      this.getState = () => this.stateMap[buySell.resolveState(this.state)];
+    }
   });
