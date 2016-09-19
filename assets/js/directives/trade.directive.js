0ea90381dac3d4749e0773f1a76ec91fbd2f53f9
plondon <plondon@gm.slc.edu>
2016-09-19 16:02:06 -0400

    fix(Trades): dont fetch/show btc expected for completed trades

--- assets/js/directives/trade.directive.js
+++ assets/js/directives/trade.directive.js
@@ -18,21 +18,16 @@ function trade ($rootScope, Alerts, MyWallet, $timeout, $interval, buySell) {
   return directive;
 
   function link (scope, elem, attrs) {
-    let errorStates = ['cancelled', 'expired', 'rejected'];
-    let successStates = ['completed', 'completed_test'];
-    let pendingStates = ['awaiting_transfer_in', 'processing', 'reviewing'];
-    let completedStates = ['expired', 'rejected', 'cancelled', 'completed', 'completed_test'];
-
     let fifteenMinutesAgo = new Date(new Date().getTime() - 15 * 60 * 1000);
     scope.expiredQuote = fifteenMinutesAgo > scope.trade.createdAt;
 
     scope.status = {};
 
     scope.update = () => angular.extend(scope, {
-      error: errorStates.indexOf(scope.trade.state) > -1,
-      success: successStates.indexOf(scope.trade.state) > -1,
-      pending: pendingStates.indexOf(scope.trade.state) > -1,
-      completed: completedStates.indexOf(scope.trade.state) > -1
+      error: buySell.errorStates.indexOf(scope.trade.state) > -1,
+      success: buySell.watchableStates.indexOf(scope.trade.state) > -1,
+      pending: buySell.pendingStates.indexOf(scope.trade.state) > -1,
+      completed: buySell.completedStates.indexOf(scope.trade.state) > -1
     });
 
     scope.cancel = (trade) => {
@@ -58,8 +53,8 @@ function trade ($rootScope, Alerts, MyWallet, $timeout, $interval, buySell) {
     scope.$watch('expiredQuote', (newVal) => {
       if (newVal) {
         scope.update();
-        scope.status.gettingQuote = true;
-        scope.trade.btcExpected().then(updateBTCExpected);
+        !scope.completed && (scope.status.gettingQuote = true);
+        !scope.completed && scope.trade.btcExpected().then(updateBTCExpected);
       }
     });
 
