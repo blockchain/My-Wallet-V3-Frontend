c6a998c51c2e5164d254c0d18dd893e1634cf37e
Justin Tormey <justin@blockchain.com>
2016-10-11 16:18:29 +0100

    refactor(ConfirmRecovery): clean confirm recovery phrase code

--- assets/js/controllers/confirmRecoveryPhrase.controller.js
+++ assets/js/controllers/confirmRecoveryPhrase.controller.js
@@ -2,105 +2,50 @@ angular
   .module('walletApp')
   .controller('ConfirmRecoveryPhraseCtrl', ConfirmRecoveryPhraseCtrl);
 
-function ConfirmRecoveryPhraseCtrl ($scope, $log, Wallet, Alerts, $uibModalInstance, $translate) {
+function ConfirmRecoveryPhraseCtrl ($scope, $uibModalInstance, Wallet, Alerts) {
   $scope.step = 0;
   $scope.offset = 0;
-  $scope.recoveryPhrase = null;
+  $scope.recoveryPhrase = [];
+  $scope.words = [...Array(4)].map(_ => ({}));
 
-  $scope.lastWordGroup = false;
-
-  $scope.words = [
-    {
-      value: '',
-      actual: '',
-      index: null,
-      error: null
-    }, {
-      value: '',
-      actual: '',
-      index: null,
-      error: null
-    }, {
-      value: '',
-      actual: '',
-      index: null,
-      error: null
-    }, {
-      value: '',
-      actual: '',
-      index: null,
-      error: null
-    }
-  ];
-
-  $scope.setRandomWords = recoveryPhrase => {
+  $scope.setRandomWords = () => {
     let currentWordIndex = 0;
     while (currentWordIndex < 4) {
       const randIndex = $scope.getRandInRange(0, 11);
-      const randWord = recoveryPhrase.split(' ').splice(randIndex, 1).toString();
-      if ($scope.hasWordBeenUsed(randIndex + 1)) {
-        continue;
-      }
+      const randWord = $scope.recoveryPhrase.slice(randIndex)[0];
+      if ($scope.hasWordBeenUsed(randIndex + 1)) { continue; }
       let word = $scope.words[currentWordIndex++];
       word.actual = randWord;
       word.index = randIndex + 1;
     }
   };
 
-  $scope.hasWordBeenUsed = index => {
-    return $scope.words.some(e => e.index === index);
-  };
+  $scope.hasWordBeenUsed = (index) => (
+    $scope.words.some(e => e.index === index)
+  );
 
-  $scope.getRandInRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+  $scope.getRandInRange = (min, max) => (
+    Math.floor(Math.random() * (max - min + 1) + min)
+  );
 
-  $scope.close = () => {
-    $uibModalInstance.dismiss('');
-  };
-
-  $scope.goToVerify = () => {
-    $scope.step = 2;
-  };
+  $scope.hasEmptyWords = () => (
+    $scope.words.some(word => word.value === '')
+  );
 
   $scope.goToShow = () => {
     $scope.step = 1;
 
-    if ($scope.mnemonic == null) {
-      const success = mnemonic => {
-        $scope.recoveryPhrase = mnemonic.split(' ');
-        $scope.setRandomWords(mnemonic);
-      };
+    let success = (mnemonic) => {
+      $scope.recoveryPhrase = mnemonic.split(' ');
+      $scope.setRandomWords();
+    };
 
-      const error = error => {
-        Alerts.displayError(error);
-        $uibModalInstance.dismiss('');
-      };
+    let error = (error) => {
+      Alerts.displayError(error);
+      $scope.close();
+    };
 
-      const cancel = () => {
-        $uibModalInstance.dismiss('');
-      };
-
-      Wallet.getMnemonic(success, error, cancel);
-    }
-  };
-
-  $scope.hasEmptyWords = () => $scope.words.some((word) => word.value === '');
-
-  $scope.nextWords = () => {
-    if ($scope.offset >= 8) return;
-    $scope.offset += 4;
-    if ($scope.offset === 8) $scope.lastWordGroup = true;
-  };
-
-  $scope.previousWords = () => {
-    if ($scope.offset <= 0) return;
-    $scope.offset -= 4;
-    $scope.lastWordGroup = false;
-  };
-
-  $scope.previousStep = () => {
-    $scope.step -= 1;
+    Wallet.getMnemonic(success, error, $scope.close);
   };
 
   $scope.verify = () => {
@@ -114,4 +59,14 @@ function ConfirmRecoveryPhraseCtrl ($scope, $log, Wallet, Alerts, $uibModalInsta
       $scope.step = 3;
     }
   };
+
+  $scope.close = () => { $uibModalInstance.dismiss(''); };
+
+  $scope.previousStep = () => { $scope.step -= 1; };
+
+  $scope.goToVerify = () => { $scope.step = 2; };
+
+  $scope.nextWords = () => { $scope.offset += 4; };
+
+  $scope.previousWords = () => { $scope.offset -= 4; };
 }
