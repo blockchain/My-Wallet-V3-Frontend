dc77997a674ea6967afddc12c0b4fb0b9dd1cb3c
Justin Tormey <justin@blockchain.com>
2016-10-17 11:12:09 +0100

    feat(Email): warn against using mail providers reported as troublesome

--- assets/js/controllers/settings/changeEmail.controller.js
+++ assets/js/controllers/settings/changeEmail.controller.js
@@ -2,11 +2,25 @@ angular
   .module('walletApp')
   .controller('ChangeEmailCtrl', ChangeEmailCtrl);
 
+const problemProviders = [
+  'aol.com',
+  'yandex.ru',
+  'hushmail.com',
+  'mail2tor.com',
+  'safe-mail.net',
+  'lelantos.org'
+];
+
 function ChangeEmailCtrl ($scope, Wallet, MyWallet) {
   // TODO: add function to My-Wallet-V3 to check if the user has any exchange account:
   $scope.userHasExchangeAcct = MyWallet.wallet.external &&
                                MyWallet.wallet.external.coinify &&
                                MyWallet.wallet.external.coinify.user;
+
+  $scope.isProblemProvider = (email) => {
+    let provider = email && email.split('@')[1] || false;
+    return provider && problemProviders.some(p => p === provider);
+  };
 
   $scope.reset = () => {
     $scope.fields = { email: Wallet.user.email };
