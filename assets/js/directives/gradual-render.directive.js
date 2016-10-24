2e02781053c3eac359c2ea7e512803f8030edfc4
Justin Tormey <justin@blockchain.com>
2016-10-24 17:50:04 -0400

    fix(Accounts): use gradual rendering for imported addresses

---
+++ assets/js/directives/gradual-render.directive.js
@@ -0,0 +1,19 @@
+
+angular
+  .module('walletApp')
+  .directive('gradualRender', gradualRender);
+
+function gradualRender () {
+  return {
+    restrict: 'AE',
+    controllerAs: '$render',
+    controller: gradualRenderController
+  };
+}
+
+function gradualRenderController () {
+  const defaultStartCount = 20;
+  const defaultStepSize = 10;
+  this.limit = defaultStartCount;
+  this.next = () => { this.limit += defaultStepSize; };
+}
