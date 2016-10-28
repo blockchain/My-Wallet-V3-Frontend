939d6e9d2067d12fd7b009cec2b12bc792b2d2c4
Justin Tormey <justin@blockchain.com>
2016-10-28 13:24:24 -0400

    feat(SFOX): fill out select partner view according to design

--- assets/js/controllers/buySellSelectPartner.controller.js
+++ assets/js/controllers/buySellSelectPartner.controller.js
@@ -8,12 +8,18 @@ function BuySellSelectPartnerController ($scope, $state, country, MyWallet) {
   if (MyWallet.wallet.external.coinify.user) $state.go(base + '.coinify');
   if (MyWallet.wallet.external.sfox.user) $state.go(base + '.sfox');
 
+  $scope.country = null;
   $scope.countries = ['USA', 'Other'];
 
+  $scope.getPartner = (country) => (
+    country === 'USA'
+      ? { name: 'SFOX', icon: '🐕 SFOX', route: '.sfox' }
+      : { name: 'Coinify', icon: 'Coinify', route: '.coinify' }
+  );
+
   $scope.selectPartner = (country) => {
-    let isUSA = country === 'USA';
-    let partner = base + (isUSA ? '.sfox' : '.coinify');
-    $state.go(partner);
+    let partner = $scope.getPartner($scope.country);
+    $state.go(base + partner.route);
   };
 
   if (country != null) {
