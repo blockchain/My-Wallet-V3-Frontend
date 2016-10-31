927ca2b75493394e67b9d6c076ef31697b882e0b
plondon <plondon@gm.slc.edu>
2016-10-31 17:44:38 -0400

    feat(SFOX): update bank link view

--- assets/js/controllers/sfox/sfoxLink.controller.js
+++ assets/js/controllers/sfox/sfoxLink.controller.js
@@ -2,5 +2,43 @@ angular
   .module('walletApp')
   .controller('SfoxLinkController', SfoxLinkController);
 
-function SfoxLinkController () {
+function SfoxLinkController ($scope, $q) {
+  let exchange = $scope.vm.exchange;
+
+  let state = $scope.state = {
+    busy: false,
+    terms: false
+  };
+
+  $scope.fields = {
+    nickname: undefined,
+    routingNumber: undefined,
+    accountNumber: undefined,
+    type: 'checking'
+  };
+
+  $scope.link = () => {
+    $scope.lock();
+
+    $q.resolve(exchange.getBuyQuote(1, 'USD', 'BTC'))
+      .then((quote) => quote.getPaymentMediums())
+      .then((mediums) => addAccount(mediums))
+      .then((account) => $scope.account = account)
+      .catch(error => console.error(error))
+      .finally($scope.free);
+
+    let addAccount = (mediums) => {
+      // routingNumber, accountNumber, name, nickname, type
+      return mediums.ach.addAccount($scope.fields.routingNumber,
+                                    $scope.fields.accountNumber,
+                                    'name',
+                                    $scope.fields.nickname,
+                                    $scope.fields.type);
+    };
+  };
+
+  $scope.lock = () => { state.busy = true; };
+  $scope.free = () => { state.busy = false; };
+
+  $scope.types = ['checking', 'savings'];
 }
--- assets/js/controllers/sfox/sfoxSignup.controller.js
+++ assets/js/controllers/sfox/sfoxSignup.controller.js
@@ -8,5 +8,6 @@ function SfoxSignupController ($stateParams) {
   this.steps = enumify('create', 'verify', 'link', 'buy');
   this.onStep = (s) => this.steps[s] === this.step;
   this.goTo = (s) => { this.step = this.steps[s]; };
-  this.goTo('create');
+  // this.goTo('create');
+  this.goTo('link');
 }
