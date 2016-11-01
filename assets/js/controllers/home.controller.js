86fff7457856eea301ae2723e6a66946c4af18d5
Justin Tormey <justin@blockchain.com>
2016-11-01 13:36:50 -0400

    fix(Home): remove imported address total row from home balances

--- assets/js/controllers/home.controller.js
+++ assets/js/controllers/home.controller.js
@@ -4,21 +4,16 @@ angular
 
 function HomeCtrl ($scope, Wallet, $uibModal) {
   $scope.getTotal = () => Wallet.total('');
-  $scope.getLegacyTotal = () => Wallet.total('imported');
 
-  $scope.activeLegacyAddresses = () => {
-    if (Wallet.status.isLoggedIn) {
-      return Wallet.legacyAddresses().filter(a => !a.archived);
-    } else {
-      return null;
-    }
-  };
+  $scope.activeLegacyAddresses = () => (
+    Wallet.status.isLoggedIn
+      ? Wallet.legacyAddresses().filter(a => !a.archived)
+      : null
+  );
 
-  $scope.activeAccounts = () => {
-    if (Wallet.status.isLoggedIn) {
-      return Wallet.accounts().filter(a => !a.archived);
-    } else {
-      return null;
-    }
-  };
+  $scope.activeAccounts = () => (
+    Wallet.status.isLoggedIn
+      ? Wallet.accounts().filter(a => !a.archived)
+      : null
+  );
 }
