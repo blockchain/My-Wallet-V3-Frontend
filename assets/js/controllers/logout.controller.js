f1eb8dcceb7174c7897cb6c25d2c8ec8a141eb6f
Justin Tormey <justin@blockchain.com>
2016-11-01 15:34:25 -0400

    feat(Logout): show success page after deauth, auto redirect

--- assets/js/controllers/logout.controller.js
+++ assets/js/controllers/logout.controller.js
@@ -3,22 +3,24 @@ angular
   .module('walletApp')
   .controller('LogoutController', LogoutController);
 
-function LogoutController ($scope, $cookies, $q, $interval, $state, Wallet, Alerts) {
+function LogoutController ($scope, $cookies, $q, $timeout, $interval, $state, Wallet, Alerts) {
+  $scope.timeToRedirect = 10;
+  $scope.toLogin = () => { $state.go('public.login-no-uid'); };
+
   $scope.deauth = () => {
     $scope.deauthorizing = true;
+    $scope.timeToRedirect = -1;
     let sessionToken = $cookies.get('session');
     $cookies.remove('session');
 
     $q.resolve(Wallet.my.endSession(sessionToken))
       .then(() => $scope.deauthorized = true)
       .catch(() => { Alerts.displayError('ERROR_DEAUTH'); })
-      .finally(() => $scope.deauthorizing = false);
+      .finally(() => $scope.deauthorizing = false)
+      .then(() => $timeout($scope.toLogin, 3000));
   };
 
-  $scope.timeToRedirect = 10;
   $interval(() => {
-    if (--$scope.timeToRedirect === 0) {
-      $state.go('public.login-no-uid');
-    }
+    if (--$scope.timeToRedirect === 0) $scope.toLogin();
   }, 1000);
 }
