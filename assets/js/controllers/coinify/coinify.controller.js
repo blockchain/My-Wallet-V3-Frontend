b1329223e7565f8ef13d615792629b1f6f2dbc98
Justin Tormey <justin@blockchain.com>
2016-12-13 10:49:01 -0500

    fix(Coinify): use verification link instead of code

--- assets/js/controllers/coinify/coinify.controller.js
+++ assets/js/controllers/coinify/coinify.controller.js
@@ -55,7 +55,7 @@ function CoinifyController ($scope, $filter, $q, MyWallet, Wallet, MyWalletHelpe
   $scope.formattedTrade = undefined;
   $scope.bitcoinReceived = buyOptions.bitcoinReceived && $scope.trade && $scope.trade.bitcoinReceived;
 
-  $scope.fields = { email: $scope.user.email, emailVerification: undefined };
+  $scope.fields = { email: $scope.user.email };
 
   $scope.transaction = trade == null
     ? ({ fiat: buyOptions.fiat, btc: buyOptions.btc, fee: 0, total: 0, currency: buyOptions.currency || buySell.getCurrency() })
@@ -199,7 +199,7 @@ function CoinifyController ($scope, $filter, $q, MyWallet, Wallet, MyWalletHelpe
 
   $scope.isDisabled = () => {
     if ($scope.onStep('email')) {
-      return !$scope.fields.emailVerification;
+      return !$scope.user.isEmailVerified;
     } else if ($scope.onStep('accept-terms')) {
       return !$scope.signupForm.$valid;
     } else if ($scope.onStep('select-payment-medium')) {
