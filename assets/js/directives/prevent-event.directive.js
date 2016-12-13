892e8f9618d85b0b17b3713a624cf04477d858f1
Justin Tormey <justin@blockchain.com>
2016-12-13 13:27:56 -0500

    feat(Directive): directive to prevent element events from eventing

---
+++ assets/js/directives/prevent-event.directive.js
@@ -0,0 +1,16 @@
+angular
+  .module('walletApp')
+  .directive('preventEvent', preventEvent);
+
+function preventEvent () {
+  return {
+    restrict: 'A',
+    scope: false,
+    link
+  };
+
+  function link (scope, elem, attrs) {
+    let eventToPrevent = attrs.preventEvent;
+    elem.on(eventToPrevent, (event) => event.preventDefault());
+  }
+}
