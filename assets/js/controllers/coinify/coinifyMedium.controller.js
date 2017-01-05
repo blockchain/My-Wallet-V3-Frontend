5bd6801b3ea14517965d7d6588eebee3cc693758
Philip Welber <welber@blockchain.com>
2017-01-05 15:07:50 -0500

    fix(coinify payment method): adds credit card logos

--- assets/js/controllers/coinify/coinifyMedium.controller.js
+++ assets/js/controllers/coinify/coinifyMedium.controller.js
@@ -5,7 +5,7 @@ angular
 function CoinifyMediumController ($scope, Alerts, buySell) {
   $scope.$parent.medium = $scope.trade ? $scope.trade.medium : undefined;
   $scope.$parent.mediums = {};
-
+  
   $scope.$parent.getMedium = () => $scope.mediums[$scope.medium] || {};
   $scope.$parent.isMedium = (medium) => $scope.getMedium().inMedium === medium;
 
