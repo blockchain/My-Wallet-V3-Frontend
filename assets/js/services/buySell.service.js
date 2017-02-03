8ae1ae6d911d24c3b3060f0e1dc0eedf85c7e339
Justin Tormey <justin@blockchain.com>
2017-02-03 15:02:43 -0500

    fix(Mobile): use tray for trade details on mobile and do not show buy cta

--- assets/js/services/buySell.service.js
+++ assets/js/services/buySell.service.js
@@ -2,7 +2,7 @@ angular
   .module('walletApp')
   .factory('buySell', buySell);
 
-function buySell ($rootScope, $timeout, $q, $state, $uibModal, $uibModalStack, Wallet, MyWallet, MyWalletHelpers, Alerts, currency, MyWalletBuySell, Options) {
+function buySell ($rootScope, $timeout, $q, $state, $uibModal, $uibModalStack, Wallet, MyWallet, MyWalletHelpers, Alerts, currency, MyWalletBuySell, Options, modals) {
   let states = {
     error: ['expired', 'rejected', 'cancelled'],
     success: ['completed', 'completed_test'],
@@ -239,7 +239,7 @@ function buySell ($rootScope, $timeout, $q, $state, $uibModal, $uibModalStack, W
   }
 
   function openBuyView (trade = null, options = {}) {
-    return $uibModal.open({
+    let config = {
       templateUrl: 'partials/coinify-modal.jade',
       windowClass: 'bc-modal auto buy',
       controller: 'CoinifyController',
@@ -249,7 +249,10 @@ function buySell ($rootScope, $timeout, $q, $state, $uibModal, $uibModalStack, W
         trade: () => trade && trade.refresh().then(() => trade),
         buyOptions: () => options
       }
-    }).result;
+    };
+    let modalInstance = $rootScope.inMobileBuy
+      ? modals.expandTray(config) : $uibModal.open(config);
+    return modalInstance.result;
   }
 
   function getCurrency (trade) {
