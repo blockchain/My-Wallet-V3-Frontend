517f35958e5223527d33b4860a8fd09364a543ac
Sjors Provoost <sjors@sprovoost.nl>
2017-04-18 17:13:10 +0200

    refactor(Linter): whats new constant file

--- assets/js/wallet-app.module.js
+++ assets/js/wallet-app.module.js
@@ -76,14 +76,6 @@ angular.module('walletApp', modules)
     }]
   });
 })
-.constant('whatsNew', [
-  { title: 'BUY_BITCOIN', desc: 'BUY_BITCOIN_EXPLAIN', date: new Date('12/15/2016'), ref: 'wallet.common.buy-sell' },
-  { title: 'EXPORT_HISTORY', desc: 'EXPORT_HISTORY_EXPLAIN', date: 1466521300000 },
-  { title: 'WHATS_NEW', desc: 'WHATS_NEW_EXPLAIN', date: 1463716800000 },
-  { title: 'SIGN_VERIFY', desc: 'SIGN_VERIFY_EXPLAIN', date: 1462161600000 },
-  { title: 'TRANSFER_ALL', desc: 'TRANSFER_ALL_EXPLAIN', date: 1461556800000 },
-  { title: 'DEV_THEMES', desc: 'DEV_THEMES_EXPLAIN', date: 1474862400000 }
-])
 .run(($rootScope, $window, $uibModal, $state, $q, $timeout, $location, languages) => {
   $rootScope.$safeApply = (scope = $rootScope, before) => {
     before = before;
