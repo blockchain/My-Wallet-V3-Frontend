ac8ee363b247845a77471f0d2f65c929965da0bf
Phil London <phillondon@abcd1234.local>
2017-04-24 17:28:28 -0400

    refactor(Coinify): use timeToExpiration for more accurate quote countdown

--- assets/js/directives/quote-countdown.directive.js
+++ assets/js/directives/quote-countdown.directive.js
@@ -18,20 +18,20 @@ function quoteCountdown ($interval) {
   return directive;
 
   function link (scope, elem, attrs) {
+    let timeToExpiration = scope.quote.timeToExpiration;
+
     scope.counter = $interval(() => {
       if (!scope.quote.id) return;
 
-      let now = new Date();
-      let expiresAt = new Date(scope.quote.expiresAt);
-
-      let diff = expiresAt - now;
-      let time = diff / 1000 / 60;
+      let time = timeToExpiration / 1000 / 60;
       let minutes = parseInt(time, 10);
       let seconds = parseInt((time % 1) * 60, 10);
       if (seconds < 10) seconds = '0' + seconds;
       if (time <= 0) scope.refreshQuote();
 
-      scope.count = time <= 0 ? '0:00' : minutes + ':' + seconds;
+      timeToExpiration -= 1000;
+
+      scope.count = timeToExpiration <= 0 ? '0:00' : minutes + ':' + seconds;
     }, 1000);
 
     scope.cancelCounter = () => $interval.cancel(scope.counter);
