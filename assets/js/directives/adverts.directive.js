072eb7713af61faf67914659bd4d04996e194ccf
Sjors Provoost <sjors@sprovoost.nl>
2017-04-25 13:17:43 +0100

    fix(Adverts): use Env service

--- assets/js/directives/adverts.directive.js
+++ assets/js/directives/adverts.directive.js
@@ -3,9 +3,9 @@ angular
   .module('walletApp')
   .directive('adverts', adverts);
 
-adverts.$inject = ['Adverts', '$rootScope'];
+adverts.$inject = ['Adverts', 'Env'];
 
-function adverts (Adverts, $rootScope) {
+function adverts (Adverts, Env) {
   const directive = {
     restrict: 'E',
     replace: 'true',
@@ -16,8 +16,10 @@ function adverts (Adverts, $rootScope) {
   return directive;
 
   function link (scope, elem, attrs) {
+    Env.then(env => {
+      scope.baseUrl = env.apiDomain + 'ads/out?id=';
+    });
     scope.ads = Adverts.ads;
-    scope.baseUrl = $rootScope.apiDomain + 'ads/out?id=';
     Adverts.fetchOnce();
   }
 }
