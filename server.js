20a75b43f2e869d591948775a89e11d88b32cebf
Sjors Provoost <sjors@sprovoost.nl>
2017-04-26 12:00:26 +0100

    dev(WalletHelper): allow custom URL
    This won't actually work yet due to restricted frame-ancestor header.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ loadEnv('.env');
 
 var port = parseInt(process.env.PORT, 10) || 8080;
 var walletHelperPort = port + 1;
+var runWalletHelper = !Boolean(process.env.WALLET_HELPER_URL);
 var dist = parseInt(process.env.DIST, 10) === 1;
 var rootURL = process.env.ROOT_URL || 'https://blockchain.info';
 var webSocketURL = process.env.WEB_SOCKET_URL || false;
@@ -23,10 +24,13 @@ var testnet = process.env.NETWORK === 'testnet';
 // App configuration
 var rootApp = express();
 var app = express();
-var helperApp = express();
+var helperApp = runWalletHelper ? express() : null;
 
 app.use(compression());
-helperApp.use(compression());
+
+if (runWalletHelper) {
+  helperApp.use(compression());
+}
 
 rootApp.use('/:lang?/wallet', app);
 
@@ -80,52 +84,54 @@ app.use(function (req, res, next) {
   next();
 });
 
-helperApp.use(function (req, res, next) {
-  var cspHeader;
-  if (req.url === '/wallet-helper/plaid/') {
-    cspHeader = ([
-      "img-src 'none'",
-      "style-src 'self'",
-      'child-src https://cdn.plaid.com',
-      'frame-src https://cdn.plaid.com',
-      'frame-ancestors http://localhost:8080',
-      "script-src 'self' https://cdn.plaid.com https://ajax.googleapis.com",
-      "connect-src 'none'",
-      "object-src 'none'",
-      "media-src 'none'",
-      "font-src 'self'"
-    ]).join('; ');
-    res.setHeader('content-security-policy', cspHeader);
-    // Not supported in Chrome, so using frame-ancestors instead
-    // res.setHeader('X-Frame-Options', 'ALLOW-FROM http://localhost:8080/');
-    res.render('plaid/index.html');
-    return;
-  } else if (req.url === '/wallet-helper/sift-science/') {
-    cspHeader = ([
-      'img-src https://hexagon-analytics.com',
-      "style-src 'none'",
-      "child-src 'none'",
-      "frame-src 'none'",
-      'frame-ancestors http://localhost:8080',
-      "script-src 'self' https://ajax.googleapis.com https://cdn.siftscience.com",
-      "connect-src 'none'",
-      "object-src 'none'",
-      "media-src 'none'",
-      "font-src 'none'"
-    ]).join('; ');
-    res.setHeader('content-security-policy', cspHeader);
-    // Not supported in Chrome, so using frame-ancestors instead
-    // res.setHeader('X-Frame-Options', 'ALLOW-FROM http://localhost:8080/');
-    res.render('sift-science/index.html');
-    return;
-  }
-  if (dist) {
-    res.setHeader('Cache-Control', 'public, max-age=31557600');
-  } else {
-    res.setHeader('Cache-Control', 'public, max-age=0, no-cache');
-  }
-  next();
-});
+if (runWalletHelper) {
+  helperApp.use(function (req, res, next) {
+    var cspHeader;
+    if (req.url === '/wallet-helper/plaid/') {
+      cspHeader = ([
+        "img-src 'none'",
+        "style-src 'self'",
+        'child-src https://cdn.plaid.com',
+        'frame-src https://cdn.plaid.com',
+        'frame-ancestors http://localhost:8080',
+        "script-src 'self' https://cdn.plaid.com https://ajax.googleapis.com",
+        "connect-src 'none'",
+        "object-src 'none'",
+        "media-src 'none'",
+        "font-src 'self'"
+      ]).join('; ');
+      res.setHeader('content-security-policy', cspHeader);
+      // Not supported in Chrome, so using frame-ancestors instead
+      // res.setHeader('X-Frame-Options', 'ALLOW-FROM http://localhost:8080/');
+      res.render('plaid/index.html');
+      return;
+    } else if (req.url === '/wallet-helper/sift-science/') {
+      cspHeader = ([
+        'img-src https://hexagon-analytics.com',
+        "style-src 'none'",
+        "child-src 'none'",
+        "frame-src 'none'",
+        'frame-ancestors http://localhost:8080',
+        "script-src 'self' https://ajax.googleapis.com https://cdn.siftscience.com",
+        "connect-src 'none'",
+        "object-src 'none'",
+        "media-src 'none'",
+        "font-src 'none'"
+      ]).join('; ');
+      res.setHeader('content-security-policy', cspHeader);
+      // Not supported in Chrome, so using frame-ancestors instead
+      // res.setHeader('X-Frame-Options', 'ALLOW-FROM http://localhost:8080/');
+      res.render('sift-science/index.html');
+      return;
+    }
+    if (dist) {
+      res.setHeader('Cache-Control', 'public, max-age=31557600');
+    } else {
+      res.setHeader('Cache-Control', 'public, max-age=0, no-cache');
+    }
+    next();
+  });
+}
 
 rootApp.use(function (req, res, next) {
   if (req.url === '/') {
@@ -137,7 +143,9 @@ rootApp.use(function (req, res, next) {
   }
 });
 
-helperApp.engine('html', ejs.renderFile);
+if (runWalletHelper) {
+  helperApp.engine('html', ejs.renderFile);
+}
 
 if (dist) {
   console.log('Production mode: single javascript file, cached');
@@ -145,16 +153,20 @@ if (dist) {
   app.use(express.static('dist'));
   app.set('views', path.join(__dirname, 'dist'));
 
-  helperApp.use('/wallet-helper', express.static('dist/wallet-helper'));
-  helperApp.set('views', path.join(__dirname, 'dist/wallet-helper'));
+  if (runWalletHelper) {
+    helperApp.use('/wallet-helper', express.static('dist/wallet-helper'));
+    helperApp.set('views', path.join(__dirname, 'dist/wallet-helper'));
+  }
 } else {
   console.log('Development mode: multiple javascript files, not cached');
   app.use(express.static(__dirname));
   app.set('view engine', 'pug');
   app.set('views', __dirname);
 
-  helperApp.use('/wallet-helper', express.static(path.join(__dirname, 'helperApp/build')));
-  helperApp.set('views', path.join(__dirname, 'helperApp/build'));
+  if (runWalletHelper) {
+    helperApp.use('/wallet-helper', express.static(path.join(__dirname, 'helperApp/build')));
+    helperApp.set('views', path.join(__dirname, 'helperApp/build'));
+  }
 }
 
 rootApp.use(express.static(__dirname + '/rootApp'));
@@ -168,17 +180,23 @@ app.use(function (req, res) {
   res.status(404).send('<center><h1>404 Not Found</h1></center>');
 });
 
-helperApp.use(function (req, res) {
-  res.status(404).send('<center><h1>404 Not Found</h1></center>');
-});
+if (runWalletHelper) {
+  helperApp.use(function (req, res) {
+    res.status(404).send('<center><h1>404 Not Found</h1></center>');
+  });
+}
 
 rootApp.listen(port, function () {
   console.log('Visit http://localhost:%d/', port);
 });
 
-helperApp.listen(walletHelperPort, function () {
-  console.log('Helper App running on http://localhost:%d/wallet-helper/', walletHelperPort);
-});
+if (runWalletHelper) {
+  helperApp.listen(walletHelperPort, function () {
+    console.log('Helper App running on http://localhost:%d/wallet-helper/', walletHelperPort);
+  });
+} else {
+  console.log('Connect to Helper App on %s/wallet-helper/', walletHelperFrameDomain);
+}
 
 // Helper functions
 function loadEnv (envFile) {
