72b24148adcc202a46eeb1f0d89c57a20b6d5faf
Lyncee <guillaume@blockchain.com>
2017-05-03 11:33:16 +0100

    fix(ExportHistoryController) - calendar dates were not updating filename & report / calendar endDate could be set in the future

--- assets/js/helpers/date.helper.js
+++ assets/js/helpers/date.helper.js
@@ -4,46 +4,45 @@ angular
 
 // TODO: Integrate moment.js later
 
-function DateHelper($filter) {
+function DateHelper ($filter) {
+  let epochBitcoinStartDate = 1231024500000;
 
-    let epochBitcoinStartDate = 1231024500000;
+  // Properties
+  let format = {
+    shortDate: 'dd/MM/yyyy'
+  };
 
-    // Properties
-    let format = {
-        shortDate: 'dd/MM/yyyy'
-    };
+  let bitcoinStartDate = new Date(epochBitcoinStartDate);
 
-    let bitcoinStartDate = new Date(epochBitcoinStartDate);
+  let now = () => { return new Date(); };
 
-    let now = () => { return new Date(); };
+  // Methods
+  let round = (d) => {
+    d = new Date(d);
+    d.setHours(0);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    d.setMilliseconds(0);
+    return d;
+  };
 
-    // Methods
-    let round = (d) => {
-        d = new Date(d);
-        d.setHours(0);
-        d.setMinutes(0);
-        d.setSeconds(0);
-        d.setMilliseconds(0);
-        return d;
-    };
-    
-    let subtractDays = (d, days) => {
-        d = new Date(d);
-        d.setDate(d.getDate() - days);
-        return d;
-    };
+  let subtractDays = (d, days) => {
+    d = new Date(d);
+    d.setDate(d.getDate() - days);
+    return d;
+  };
 
-    let toShortDate = (d) => $filter('date')(d, format.shortDate);
+  let toShortDate = (d) => $filter('date')(d, format.shortDate);
 
-    let toCustomShortDate = (sep, d) => $filter('date')(d, `dd${sep}MM${sep}yyyy`);
+  let toCustomShortDate = (sep, d) => $filter('date')(d, `dd${sep}MM${sep}yyyy`);
 
-    return {
-        format: format,
-        bitcoinStartDate: bitcoinStartDate,
-        now: now,
-        round: round,
-        subtractDays: subtractDays,
-        toShortDate: toShortDate,
-        toCustomShortDate: toCustomShortDate
-    }
+  return {
+    format: format,
+    bitcoinStartDate: bitcoinStartDate,
+    now: now,
+    round: round,
+    subtractDays: subtractDays,
+    toShortDate: toShortDate,
+    toCustomShortDate: toCustomShortDate
+  };
 }
