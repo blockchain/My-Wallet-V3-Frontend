8b6d4a1a00f5bfb8c29df6e127aed6b73327e526
Justin Tormey <justin@blockchain.com>
2017-05-12 14:24:48 -0400

    test(BuyMobile): test exitToNativeTx function

--- tests/controllers/coinify/coinify_ctrl_spec.js
+++ tests/controllers/coinify/coinify_ctrl_spec.js
@@ -1,10 +1,7 @@
 describe('CoinifyController', () => {
   let $rootScope;
   let $controller;
-  let options;
   let buySell;
-  let $scope;
-  let Alerts;
   let $q;
 
   let quote = {
@@ -21,54 +18,32 @@ describe('CoinifyController', () => {
     angular.mock.inject(function ($injector, $q, _$rootScope_, _$controller_) {
       $rootScope = _$rootScope_;
       $controller = _$controller_;
-      let Wallet = $injector.get('Wallet');
-      let MyWallet = $injector.get('MyWallet');
-      let currency = $injector.get('currency');
       buySell = $injector.get('buySell');
-      Alerts = $injector.get('Alerts');
-
-      options = {
-        partners: {
-          coinify: {
-            surveyLinks: ['www.blockchain.com/survey']
-          }
-        }
-      };
-
-      MyWallet.wallet = {
-        hdwallet: {
-          defaultAccount: {
-            index: 0
-          },
-          accounts: [{label: ''}]
-        }
-      };
-      return {
-        buySell: {
-          getQuote(quote) { return $q.resolve(quote); }
-        }
-      };}));
+    }));
 
   let getController = function (quote, trade, options) {
     let scope = $rootScope.$new();
 
-    return $controller("CoinifyController", {
+    let ctrl = $controller('CoinifyController', {
       $scope: scope,
       trade: trade || null,
       quote: quote || null,
       options: options || {},
-      $uibModalInstance: { close() {}, dismiss() {} }
+      $uibModalInstance: { close () {}, dismiss () {} }
     });
+
+    ctrl.$scope = scope;
+    return ctrl;
   };
 
   describe('.baseFiat()', function () {
     let ctrl;
     beforeEach(() => {
-      ctrl = getController(quote)
+      ctrl = getController(quote);
     });
 
     it('should be true if baseCurrency is fiat', () => {
-      expect(ctrl.baseFiat()).toBe(true)
+      expect(ctrl.baseFiat()).toBe(true);
     });
   });
 
@@ -139,7 +114,7 @@ describe('CoinifyController', () => {
     it('should set expired trade state', () => {
       ctrl.expireTrade();
       $rootScope.$digest();
-      return expect(ctrl.state.trade.expired).toBe(true);
+      expect(ctrl.state.trade.expired).toBe(true);
     });
   });
 
@@ -149,53 +124,54 @@ describe('CoinifyController', () => {
 
     it('should set the step', () => {
       ctrl.goTo('email');
-      return expect(ctrl.currentStep()).toBe('email');
+      expect(ctrl.currentStep()).toBe('email');
     });
   });
 
-  describe('initial state', function () {
-    beforeEach(function () { let ctrl;
-    return ctrl = undefined; });
+  describe('.exitToNativeTx()', () => {
+    it('should call mobile interface with the correct tx hash', inject((buyMobile) => {
+      let txHash = 'mock_tx_hash';
+      spyOn(buyMobile, 'callMobileInterface');
+      let ctrl = getController(null, { txHash });
+      ctrl.$scope.exitToNativeTx();
+      expect(buyMobile.callMobileInterface).toHaveBeenCalledWith(buyMobile.SHOW_TX, txHash);
+    }));
+  });
 
+  describe('initial state', function () {
     it('should ask user to verify email', inject(function (Wallet) {
       Wallet.user.isEmailVerified = false;
       buySell.getExchange = () => ({ profile: {} });
       let ctrl = getController();
-      return expect(ctrl.currentStep()).toBe('email');
-    })
-  );
+      expect(ctrl.currentStep()).toBe('email');
+    }));
 
-    it('should ask user to signup if email is verified',inject(function (Wallet) {
+    it('should ask user to signup if email is verified', inject(function (Wallet) {
       Wallet.user.isEmailVerified = true;
       let ctrl = getController();
-      return expect(ctrl.currentStep()).toBe('signup');
-    })
-    );
+      expect(ctrl.currentStep()).toBe('signup');
+    }));
 
-    it('should ask user to select payment medium',inject(function (Wallet) {
+    it('should ask user to select payment medium', inject(function (Wallet) {
       Wallet.user.isEmailVerified = true;
       buySell.getExchange = () => ({ profile: {}, user: 1 });
       let ctrl = getController(quote, null);
-      return expect(ctrl.currentStep()).toBe('select-payment-medium');
-    })
-    );
+      expect(ctrl.currentStep()).toBe('select-payment-medium');
+    }));
 
-    it('should ask user to complete isx after a trade is created',inject(function (Wallet) {
+    it('should ask user to complete isx after a trade is created', inject(function (Wallet) {
+      Wallet.user.isEmailVerified = true;
+      buySell.getExchange = () => ({ profile: {}, user: 1 });
+      let ctrl = getController(null, {});
+      expect(ctrl.currentStep()).toBe('isx');
+    }));
+
+    it('should show a completed trade summary', inject(function (Wallet) {
+      let trade = { state: 'completed' };
       Wallet.user.isEmailVerified = true;
       buySell.getExchange = () => ({ profile: {}, user: 1 });
       let ctrl = getController(null, trade);
-      return expect(ctrl.currentStep()).toBe('isx');
-    })
-    );
-
-    it('should show a completed trade summary',inject(function (Wallet) {
-      let trade =
-        {state: 'completed'};
-      Wallet.user.isEmailVerified = true;
-      buySell.getExchange = () => ({ profile: {}, user: 1 });
-      let ctrl = getController(null, trade);
-      return expect(ctrl.currentStep()).toBe('trade-complete');
-    })
-    );
+      expect(ctrl.currentStep()).toBe('trade-complete');
+    }));
   });
 });
