6e4c3e7c3a040bb1e39664a2b779c38e748bc70c
plondon <philip@blockchain.com>
2017-06-12 17:53:52 -0400

    refactor(Exchange): set and verify profile in controller

--- assets/js/controllers/sfox/sfoxVerify.controller.js
+++ assets/js/controllers/sfox/sfoxVerify.controller.js
@@ -2,7 +2,7 @@ angular
   .module('walletApp')
   .controller('SfoxVerifyController', SfoxVerifyController);
 
-function SfoxVerifyController (Env, $scope, state, sfox, modals, QA) {
+function SfoxVerifyController (Env, $q, $scope, state, sfox, modals, QA) {
   Env.then(env => {
     $scope.buySellDebug = env.buySellDebug;
   });
@@ -10,7 +10,33 @@ function SfoxVerifyController (Env, $scope, state, sfox, modals, QA) {
   $scope.exchange = $scope.vm.exchange;
   $scope.goTo = (s) => $scope.vm.goTo(s);
 
+  $scope.steps = ['address'];
+  $scope.fields = ['first', 'middle', 'last', 'ssn', 'dob', 'addr1', 'addr2', 'state-US', 'zipcode'];
+
   $scope.openHelper = modals.openHelper;
 
-  // $scope.$on('$destroy', () => { exchange.profile && exchange.profile.setSSN(null); });
+  $scope.verifyProfile = () => {
+    $q.resolve($scope.exchange.profile.verify())
+      .then(() => $scope.goTo('upload'))
+      .catch(sfox.displayError)
+      .finally($scope.free);
+  };
+
+  $scope.setProfile = (fields) => {
+    let profile = $scope.exchange.profile;
+
+    profile.firstName = fields.firstName;
+    profile.middleName = fields.middle;
+    profile.lastName = fields.lastName;
+    profile.dateOfBirth = new Date(fields.dob);
+    profile.setSSN(fields.ssn);
+
+    profile.setAddress(
+      fields.addr1,
+      fields.addr2,
+      fields.city,
+      fields.state.Code,
+      fields.zipcode
+    );
+  };
 }
