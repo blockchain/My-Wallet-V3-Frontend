234df15eaf490f6c1cd484c3183520cdd235e441
plondon <philip@blockchain.com>
2017-06-13 14:32:43 -0400

    refactor(Unocoin): remove unnecessary unocoin pending controller

--- assets/js/controllers/unocoin/unocoinPending.controller.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('walletApp')
-  .controller('UnocoinPendingController', UnocoinPendingController);
-
-function UnocoinPendingController ($scope) {
-  $scope.close = () => {
-    $scope.vm.close();
-  };
-}
