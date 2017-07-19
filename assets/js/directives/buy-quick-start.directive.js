b16851ac4837fc8ce171b4cba68ca8a43774d633
Jack Stettner <johndoe@example.com>
2017-07-19 14:06:44 -0400

    refactor(coinify-buy): removed unnecessary code

--- assets/js/directives/buy-quick-start.directive.js
+++ assets/js/directives/buy-quick-start.directive.js
@@ -32,11 +32,6 @@ function buyQuickStart ($rootScope, currency, buySell, Alerts, $interval, $timeo
     scope.inMobileBuy = $rootScope.inMobileBuy;
     scope.symbol = currency.conversions[scope.transaction.currency.code].symbol;
 
-    let exchange = buySell.getExchange();
-    this.exchange = exchange && exchange.profile ? exchange : {profile: {}};
-    this.exchangeCountry = exchange.profile ? exchange.profile.country : $stateParams.countryCode;
-    this.currency = exchange.profile ? exchange.profile.defaultCurrency : 'EUR';
-
     scope.updateLastInput = (type) => scope.lastInput = type;
     scope.isPendingTradeState = (state) => scope.pendingTrade && scope.pendingTrade.state === state && scope.pendingTrade.medium !== 'blockchain';
     scope.isPendingSellTrade = () => buySell.isPendingSellTrade(scope.pendingTrade);
