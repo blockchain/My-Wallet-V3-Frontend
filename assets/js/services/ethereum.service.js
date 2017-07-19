633ac95b6001fbe492f3a491226128e4e04f6d97
Justin Tormey <justin@blockchain.com>
2017-07-19 15:35:40 -0400

    fix(Eth): prevent blank screen on refresh

--- assets/js/services/ethereum.service.js
+++ assets/js/services/ethereum.service.js
@@ -2,6 +2,41 @@ angular
   .module('walletApp')
   .factory('Ethereum', Ethereum);
 
-function Ethereum (Wallet) {
-  return Wallet.my.wallet.eth;
+function Ethereum (MyWallet) {
+  const service = {
+    get eth () {
+      return MyWallet.wallet.eth;
+    },
+    get balance () {
+      return this.eth.balance;
+    },
+    get defaultAccount () {
+      return this.eth.defaultAccount;
+    },
+    get defaults () {
+      return this.eth.defaults;
+    }
+  };
+
+  service.getTxNote = (hash) => {
+    return this.eth.getTxNote(hash);
+  };
+
+  service.setTxNote = (hash, note) => {
+    return this.eth.setTxNote(hash, note);
+  };
+
+  service.fetchHistory = () => {
+    return this.eth.fetchHistory();
+  };
+
+  service.isContractAddress = (address) => {
+    return this.eth.isContractAddress(address);
+  };
+
+  service.isAddress = (address) => {
+    return this.eth.isAddress(address);
+  };
+
+  return service;
 }
