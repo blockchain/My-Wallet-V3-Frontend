ce49df0fdce7f8abe099167b654cf9d1cc43ab10
Philip Welber <welber@blockchain.com>
2017-07-24 11:10:04 -0400

    fix(Eth): eth tx fee

--- assets/js/components/ethereum-transaction-amount.component.js
+++ assets/js/components/ethereum-transaction-amount.component.js
@@ -17,7 +17,6 @@ function ethereumTransactionAmountController (MyBlockchainApi, Wallet, currency)
   this.settings = Wallet.settings;
   this.isBitCurrency = currency.isBitCurrency;
   this.toggleTx = () => this.showFiat = !this.showFiat;
-  this.toggleTransactionDisplayCurrency = Wallet.toggleTransactionDisplayCurrency;
   this.toggle = Wallet.toggleDisplayCurrency;
   this.absolute = (value) => Math.abs(value);
 }
