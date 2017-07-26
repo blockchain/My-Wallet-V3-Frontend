e1beca8df78617b8ba20cee07f1a33cf8b5b463d
plondon <philip@blockchain.com>
2017-07-26 13:37:56 -0400

    test(Eth): asset context

--- tests/services/assetContext.service.spec.js
+++ tests/services/assetContext.service.spec.js
@@ -14,65 +14,65 @@ describe('AssetContextService', () => {
   let hasEth = true;
 
   let configureState = ({ hasBtc = false, hasEth = false, viewing = 'home' }) => {
-    $state.current = `wallet.common.${viewing}`;
+    $state.current = { name: `wallet.common.${viewing}` };
     assetContext.hasBtcBalance = () => hasBtc;
     assetContext.hasEthBalance = () => hasEth;
   };
 
-  describe('.getBalanceContext()', () => {
+  describe('.getContext()', () => {
     describe('with BTC balance', () => {
       it('should see just btc if does not have ETH', () => {
         configureState({ hasBtc });
-        expect(assetContext.getBalanceContext()).toEqual(['btc']);
+        expect(assetContext.getContext().available).toEqual(['btc']);
       });
 
       it('should see just btc if does not have ETH and viewing /btc', () => {
         configureState({ hasBtc, viewing: 'btc' });
-        expect(assetContext.getBalanceContext()).toEqual(['btc']);
+        expect(assetContext.getContext().available).toEqual(['btc']);
       });
 
       it('should see both if has ETH', () => {
         configureState({ hasBtc, hasEth });
-        expect(assetContext.getBalanceContext()).toEqual(['btc', 'eth']);
+        expect(assetContext.getContext().available).toEqual(['btc', 'eth']);
       });
 
       it('should see both if does not have ETH but viewing /eth', () => {
         configureState({ hasBtc, viewing: 'eth' });
-        expect(assetContext.getBalanceContext()).toEqual(['btc', 'eth']);
+        expect(assetContext.getContext().available).toEqual(['btc', 'eth']);
       });
     });
 
     describe('with ETH balance', () => {
       it('should see just eth if does not have BTC', () => {
         configureState({ hasEth });
-        expect(assetContext.getBalanceContext()).toEqual(['eth']);
+        expect(assetContext.getContext().balance).toEqual('eth');
       });
 
       it('should see just eth if does not have BTC and viewing /eth', () => {
         configureState({ hasEth, viewing: 'eth' });
-        expect(assetContext.getBalanceContext()).toEqual(['eth']);
+        expect(assetContext.getContext().balance).toEqual('eth');
       });
 
       it('should see both if does not have BTC but viewing /btc', () => {
         configureState({ hasEth, viewing: 'btc' });
-        expect(assetContext.getBalanceContext()).toEqual(['btc', 'eth']);
+        expect(assetContext.getContext().available).toEqual(['btc', 'eth']);
       });
     });
 
     describe('with no balance', () => {
       it('should see just btc', () => {
         configureState({});
-        expect(assetContext.getBalanceContext()).toEqual(['btc']);
+        expect(assetContext.getContext().available).toEqual(['btc']);
       });
 
       it('should see just btc if viewing /btc', () => {
         configureState({ viewing: 'btc' });
-        expect(assetContext.getBalanceContext()).toEqual(['btc']);
+        expect(assetContext.getContext().available).toEqual(['btc']);
       });
 
       it('should see both if viewing /eth', () => {
         configureState({ viewing: 'eth' });
-        expect(assetContext.getBalanceContext()).toEqual(['btc', 'eth']);
+        expect(assetContext.getContext().available).toEqual(['btc', 'eth']);
       });
     });
   });
