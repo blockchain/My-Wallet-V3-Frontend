f36f45ca612fcae94246ad5b0e1901432f7dcc59
Jack Stettner <13202065+jstettner@users.noreply.github.com>
2017-08-08 14:10:30 -0400

    test(ShapeShift): tests cover shapeShiftCreate ctrl

--- tests/controllers/shapeshift/shapeshift_create_ctrl_spec.js
+++ tests/controllers/shapeshift/shapeshift_create_ctrl_spec.js
@@ -31,12 +31,31 @@ describe('ShapeShiftCreateController', () => {
     return $rootScope.$digest();
   });
 
+  it('should set quote handler', () => {
+    expect(scope.quoteHandler).toBeDefined();
+  });
+
+  it('should set approximate quote handler', () => {
+    expect(scope.approximateQuoteHandler).toBeDefined();
+  });
+
   describe('onComplete', () => {
     it('should go to confirm step', () => {
       spyOn(scope.vm, 'goTo');
       scope.onComplete();
       expect(scope.vm.goTo).toHaveBeenCalledWith('confirm');
     });
+    it('should set fee', () => {
+      scope.onComplete(1, 2, 3);
+      expect(scope.vm.fee).toEqual(2);
+    });
+    it('should set quote', () => {
+      scope.onComplete(1, 2, 3);
+      expect(scope.vm.quote).toEqual(3);
+    });
+    it('should set payment', () => {
+      scope.onComplete(1, 2, 3);
+      expect(scope.vm.payment).toEqual(1);
+    });
   });
-
 });
