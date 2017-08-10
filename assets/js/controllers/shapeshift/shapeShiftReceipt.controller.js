e518eba76913935c64325350317a9d01f04402e1
Jack Stettner <13202065+jstettner@users.noreply.github.com>
2017-08-10 15:00:58 -0400

    fix(ShapeShift): fixed modal confirmation at receipt

--- assets/js/controllers/shapeshift/shapeShiftReceipt.controller.js
+++ assets/js/controllers/shapeshift/shapeShiftReceipt.controller.js
@@ -2,13 +2,21 @@ angular
   .module('walletApp')
   .controller('ShapeShiftReceiptController', ShapeShiftReceiptController);
 
-function ShapeShiftReceiptController ($scope, Alerts, Env) {
+function ShapeShiftReceiptController ($scope, Alerts, Env, localStorageService) {
   let links;
   Env.then(env => links = env.shapeshift.surveyLinks);
 
   $scope.trade = $scope.vm.trade;
-
   $scope.onClose = () => {
-    $scope.vm.goTo('create');
+    let survey = 'shift-trade-survey';
+    let surveyCache = localStorageService.get(survey);
+    console.log(surveyCache);
+    let shouldClose = surveyCache && surveyCache.index === links.length - 1;
+    console.log(shouldClose);
+    if (shouldClose) {
+      $scope.vm.goTo('create');
+    } else {
+      Alerts.surveyCloseConfirm(survey, links, 1).then(() => { $scope.vm.goTo('create'); });
+    }
   };
 }
