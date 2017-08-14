142091ccd9d1ff7270afbbbacc4c2f8632c98615
Philip Welber <welber@blockchain.com>
2017-08-14 15:02:48 -0400

    refactor(Exchange): trade status and class on order history

--- assets/js/components/exchange/exchange-trade.component.js
+++ assets/js/components/exchange/exchange-trade.component.js
@@ -15,4 +15,9 @@ function ExchangeTradeController ($scope, Env, Exchange, AngularHelper, ShapeShi
   this.dateFormat = $scope.$root.size.xs ? 'MMM d' : this.dateFormat;
 
   this.trade = this.exTrade;
+  this.getClass = (trade) => {
+    if (this.trade.isFailed || this.trade.isResolved) return 'state-danger-text';
+    else if (this.trade.isComplete) return 'success';
+    else if (this.trade.isWaitingForDeposit || this.trade.isProcessing) return 'medium-blue';
+  };
 }
