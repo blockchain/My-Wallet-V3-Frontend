a1a8c5e3341d685ff2f9c56b87092fa6c858b4ec
plondon <philip@blockchain.com>
2017-08-22 13:21:31 -0400

    feat(ShapeShift): preselect state if there is one, add border and spacing

--- assets/js/components/shapeshift/shift-state-select.component.js
+++ assets/js/components/shapeshift/shift-state-select.component.js
@@ -11,20 +11,16 @@ angular
   });
 
 function shiftStateSelectController (MyWallet, Env, ShapeShift, state) {
-  Env.then(env => {
-    this.whitelisted = env.shapeshift.statesWhitelist;
-  });
-
-  this.onStateWhitelist = state => {
-    if (!state) return true;
-    return this.whitelisted.indexOf(state.Code) > -1;
-  };
+  Env.then(env => this.whitelisted = env.shapeshift.statesWhitelist);
 
   this.saveState = () => ShapeShift.setUSAState(this.state);
+  this.state = state.stateCodes.filter((s) => s.Code === ShapeShift.USAState)[0];
+  this.onStateWhitelist = () => this.state ? this.whitelisted.indexOf(this.state.Code) > -1 : true;
 
   this.signupForShift = () => {
-    let email = encodeURIComponent(this.email);
     let state = this.state.Name;
+    let email = encodeURIComponent(this.email);
+
     ShapeShift.signupForShift(email, state);
   };
 }
