b4126c41233ac7e12491ee8f1e1897c5acb2cf07
Christine Wang <christine@blockchain.com>
2017-08-23 09:44:02 -0400

    fix(confirm_exchange_order): Fix .total to this.quote.depositAmount + this.fee

--- assets/js/components/shapeshift/shift-confirm.component.js
+++ assets/js/components/shapeshift/shift-confirm.component.js
@@ -19,7 +19,7 @@ function ShiftConfirmController (AngularHelper, $scope, Exchange, Wallet, Ethere
 
   $scope.fee = this.fee;
   $scope.quote = this.quote;
-  $scope.total = quote.depositAmount + this.fee;
+  $scope.total = this.quote.depositAmount + this.fee;
 
   $scope.toSatoshi = currency.convertToSatoshi;
   $scope.bitcoin = $scope.bitcoin = currency.bitCurrencies.filter(c => c.code === 'BTC')[0];
