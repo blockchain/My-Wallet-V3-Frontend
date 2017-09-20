5d14710f731f00f55b633f1002818be123e06683
Christine Wang <christine@blockchain.com>
2017-09-20 14:23:30 -0400

    feat(ethereum_survey): Prompt ethereum survey for non-users after user with 0 ether balance closes request ether without copying ethereum address

--- assets/js/controllers/request/requestEthereum.controller.js
+++ assets/js/controllers/request/requestEthereum.controller.js
@@ -2,10 +2,15 @@ angular
   .module('walletApp')
   .controller('RequestEthereumController', RequestEthereumController);
 
-function RequestEthereumController ($scope, AngularHelper, browser, Env, Ethereum) {
+function RequestEthereumController ($scope, AngularHelper, browser, Env, Ethereum, localStorageService, Alerts) {
+  let links;
+
   Env.then(env => {
+    links = env.ethereum.surveyLinks;
     $scope.rootURL = env.rootURL;
     $scope.isProduction = env.isProduction;
+    $scope.ethereumAccount = Ethereum.defaultAccount;
+    $scope.ethereumAccount.fetchBalance().then(etherBalance => $scope.etherBalance = etherBalance.balance);
   });
 
   $scope.browser = browser;
@@ -16,7 +21,19 @@ function RequestEthereumController ($scope, AngularHelper, browser, Env, Ethereu
 
   $scope.address = () => Ethereum.defaultAccount.address;
 
+  $scope.copyEthereumAddress = () => {
+    $scope.state.isAddressCopied = true;
+    localStorageService.set('copied-ethereum-address', true);
+  };
+
+  $scope.promptEthereumSurvey = () => {
+    if (!localStorage.getItem('ls.ethereum-survey') && !localStorage.getItem('ls.copied-ethereum-address') && $scope.etherBalance === '0') {
+      Alerts.surveyCloseConfirm('ethereum-survey', links, 1, 'ETHEREUM_SURVEY_PROMPT');
+    }
+  };
+
   $scope.$watch('state.to', () => $scope.state.address = $scope.address());
+  $scope.$on('$destroy', () => $scope.promptEthereumSurvey());
 
   AngularHelper.installLock.call($scope);
 }
