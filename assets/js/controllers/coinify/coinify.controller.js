095fb0fa701e34680113ea1a81404573c2fdb6bd
plondon <philip@blockchain.com>
2017-09-20 16:49:47 -0400

    fix(Coinify): correctly display quote information

--- assets/js/controllers/coinify/coinify.controller.js
+++ assets/js/controllers/coinify/coinify.controller.js
@@ -17,12 +17,12 @@ function CoinifyController ($rootScope, $scope, $q, MyWallet, Wallet, Alerts, cu
   this.message = 'RATE_GUARANTEED';
   this.baseFiat = () => !currency.isBitCurrency({code: this.quote.baseCurrency});
   this.BTCAmount = () => !this.baseFiat() ? this.quote.baseAmount : this.quote.quoteAmount;
-  this.fiatAmount = () => this.baseFiat() ? -this.quote.baseAmount / 100 : -this.quote.quoteAmount / 100;
+  this.fiatAmount = () => this.baseFiat() ? this.quote.baseAmount : this.quote.quoteAmount;
   this.fiatCurrency = () => this.baseFiat() ? this.quote.baseCurrency : this.quote.quoteCurrency;
   this.timeToExpiration = () => this.quote ? this.quote.expiresAt - this.now() : this.trade.expiresAt - this.now();
   this.refreshQuote = () => {
-    if (this.baseFiat()) return $q.resolve(coinify.getQuote(-this.quote.baseAmount / 100, this.quote.baseCurrency)).then((q) => this.quote = q);
-    else return $q.resolve(coinify.getQuote(-this.quote.baseAmount / 100000000, this.quote.baseCurrency, this.quote.quoteCurrency)).then((q) => this.quote = q);
+    if (this.baseFiat()) return $q.resolve(coinify.getQuote(this.quote.baseAmount, this.quote.baseCurrency)).then((q) => this.quote = q);
+    else return $q.resolve(coinify.getQuote(this.quote.baseAmount / 100000000, this.quote.baseCurrency, this.quote.quoteCurrency)).then((q) => this.quote = q);
   };
   this.expireTrade = () => {
     return $q.resolve(this.state.trade.expired = true);
