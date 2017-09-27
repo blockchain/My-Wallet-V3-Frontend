90b5785be780a6f2d3ae916e10c73299e3e938c7
Philip Welber <welber@blockchain.com>
2017-09-27 11:45:53 -0400

    feat(Bitcoin Cash): filter trades, show actions only if balance

--- assets/js/components/bitcoin-cash/bitcoin-cash-wallet.component.js
+++ assets/js/components/bitcoin-cash/bitcoin-cash-wallet.component.js
@@ -12,15 +12,23 @@ angular
 function bitcoinCashWalletController (modals, ShapeShift, MyWallet, Wallet) {
   this.transactionViewOpen = false;
   this.toggleTransactionView = () => this.transactionViewOpen = !this.transactionViewOpen;
-  this.hasTransactions = () => true;
   this.balance = this.wallet.balance / 100000000;
 
   this.openSend = () => modals.openSend(null, { code: 'bch', index: this.wallet.index });
   this.openExchange = () => modals.openExchange({ code: 'bch', index: this.wallet.index });
 
   let txList = MyWallet.wallet.txList;
-  this.bcashTransactions = txList.transactions(this.wallet.index);
+  this.bchTransactions = txList.transactions(this.wallet.index);
 
   this.trades = ShapeShift.shapeshift.trades;
   this.openTradeDetails = (trade) => modals.openShiftTradeDetails(trade);
+
+  this.shiftTrades = [];
+  this.bchTransactions.forEach(tx => {
+    this.trades.filter(ss => {
+      if (ss.depositHash === tx.hash) this.shiftTrades.push(ss);
+    });
+  });
+
+  this.hasTransactions = () => this.bchTransactions.length > 0 || this.shiftTrades.length > 0;
 }
