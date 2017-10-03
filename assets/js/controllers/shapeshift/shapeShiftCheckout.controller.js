84e23a4cdb9ad45279b1a1f1fb9656e2f407ceeb
plondon <philip@blockchain.com>
2017-10-03 11:23:52 -0400

    fix(Exchange): dynamically update ss trades, only show btc, eth in order history

--- assets/js/controllers/shapeshift/shapeShiftCheckout.controller.js
+++ assets/js/controllers/shapeshift/shapeShiftCheckout.controller.js
@@ -11,15 +11,16 @@ function ShapeShiftCheckoutController ($scope, $stateParams, ShapeShift, modals,
     select (tab) { this.selectedTab = this.selectedTab ? tab : null; }
   };
 
+  this.orderHistoryCurrencies = ['btc', 'eth'];
   this.human = {'BTC': 'bitcoin', 'ETH': 'ether', 'BCH': 'bitcoin cash'};
   this.steps = enumify('state-select', 'create', 'confirm', 'receipt');
   this.onStep = (s) => this.steps[s] === this.step;
   this.goTo = (s) => this.step = this.steps[s];
 
-  this.trades = ShapeShift.shapeshift.trades;
+  this.trades = () => ShapeShift.shapeshift.trades.filter((t) => this.orderHistoryCurrencies.indexOf(t.fromCurrency.toLowerCase()) > -1);
+  this.completedTrades = () => this.trades().some(t => t.isComplete || t.isFailed || t.isResolved);
+  this.pendingTrades = () => this.trades().some(t => t.isProcessing || t.isWaitingForDeposit);
   this.openTradeDetails = (trade) => modals.openShiftTradeDetails(trade);
-  this.completedTrades = () => this.trades.some(t => t.isComplete || t.isFailed || t.isResolved);
-  this.pendingTrades = () => this.trades.some(t => t.isProcessing || t.isWaitingForDeposit);
 
   let accountInfo = MyWallet.wallet.accountInfo;
   let codeGuess = accountInfo && accountInfo.countryCodeGuess;
