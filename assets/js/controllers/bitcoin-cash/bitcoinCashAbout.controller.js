02efddf6a1484ad136024d3cca88b535dc29fa58
plondon <philip@blockchain.com>
2017-10-03 09:36:34 -0400

    fix(Bitcoin Cash): show bch balance instead of placeholder

--- assets/js/controllers/bitcoin-cash/bitcoinCashAbout.controller.js
+++ assets/js/controllers/bitcoin-cash/bitcoinCashAbout.controller.js
@@ -2,12 +2,16 @@ angular
   .module('walletApp')
   .controller('BitcoinCashAboutController', BitcoinCashAboutController);
 
-function BitcoinCashAboutController ($uibModalInstance, localStorageService, Wallet, $state) {
+function BitcoinCashAboutController ($uibModalInstance, localStorageService, MyWallet, Wallet, currency, $state) {
   let enumify = (...ns) => ns.reduce((e, n, i) => angular.merge(e, {[n]: i}), {});
 
   this.steps = enumify('about', 'balance');
   this.onStep = (s) => this.steps[s] === this.step;
   this.goTo = (s) => this.step = this.steps[s];
+
+  this.bchCurr = currency.bchCurrencies[0];
+  this.balance = MyWallet.wallet.bch.balance;
+  this.fromSatoshi = currency.convertFromSatoshi;
 
   this.goTo('about');
   this.dismiss = () => $uibModalInstance.dismiss();
