48b9effed471842408d31c06055a5738dd5c9135
plondon <philip@blockchain.com>
2017-10-04 12:27:05 -0400

    feat(Nav): handle web and expand/collapse logic

--- assets/js/sharedDirectives/public-header.directive.js
+++ assets/js/sharedDirectives/public-header.directive.js
@@ -2,32 +2,32 @@ angular
   .module('shared')
   .directive('publicHeader', publicHeader);
 
-publicHeader.$inject = ['$location', 'Env', 'languages'];
+publicHeader.$inject = ['$rootScope', '$location', '$window', 'AngularHelper', 'Env', 'languages'];
 
-function publicHeader ($location, Env, languages) {
+function publicHeader ($rootScope, $location, $window, AngularHelper, Env, languages) {
   const directive = {
     restrict: 'E',
     replace: true,
     template: `
     <div class="wrapper" data-preflight-tag="PublicHeader">
-      <nav role="navigation">
+      <nav role="navigation" ng-class="{'open': state.open, 'searching': state.searching, 'scrolling': state.scrolling}">
         <ul class="igation">
           <li>
             <a class="bc-logo" href="{{rootURL}}">
               <img src="img/white-blockchain.svg" alt="Blockchain" />
             </a>
-            <button class="menu-button">
+            <button class="menu-button" ng-click="expandHeader()">
     					<span></span>
     				</button>
-    				<button class="search-button" type="button"></button>
+    				<button class="search-button" type="button" ng-click="handleSearch()"></button>
           </li>
-          <li class="with-children">
+          <li class="with-children" ng-mouseenter="!size().mobile && expandHeader()" ng-mouseleave="collapseHeader()">
             <a href="{{rootURL}}wallet" id="wallet-link">Wallet</a>
             <ul>
               <li><a href="{{rootURL}}wallet/#/login">Login</a></li>
             </ul>
           </li>
-          <li class="with-children">
+          <li class="with-children" ng-mouseenter="!size().mobile && expandHeader()" ng-mouseleave="collapseHeader()">
             <a href="https://blockchain.info">Data</a>
             <ul>
               <li><a href="{{rootURL}}charts">Charts</a></li>
@@ -35,13 +35,13 @@ function publicHeader ($location, Env, languages) {
               <li><a href="{{rootURL}}markets">Markets</a></li>
             </ul>
           </li>
-          <li class="with-children">
+          <li class="with-children" ng-mouseenter="!size().mobile && expandHeader()" ng-mouseleave="collapseHeader()">
             <a href="{{rootURL}}api">API</a>
             <ul>
               <li><a href="https://www.blockchain.com/enterprise">Business</a></li>
             </ul>
           </li>
-          <li class="with-children">
+          <li class="with-children" ng-mouseenter="!size().mobile && expandHeader()" ng-mouseleave="collapseHeader()">
             <a href="https://www.blockchain.com/about">About</a>
             <ul>
               <li><a href="https://www.blockchain.com/team">Team</a></li>
@@ -64,6 +64,7 @@ function publicHeader ($location, Env, languages) {
     </div>
     `,
     scope: {
+      posY: '=',
       i18nLoaded: '='
     },
     link: link
@@ -79,5 +80,41 @@ function publicHeader ($location, Env, languages) {
     scope.$watch(languages.get, (code) => {
       scope.searchUrl = code === 'en' ? '/search' : `/${code}/search`;
     });
+
+    scope.size = () => $rootScope.size;
+
+    scope.expandHeader = () => {
+      scope.state.searching = false;
+      if (scope.size().mobile) {
+        scope.state.searching = false;
+        scope.state.open = !scope.state.open;
+      } else {
+        scope.state.open = true;
+      }
+    };
+
+    scope.collapseHeader = () => {
+      if (scope.size().mobile) {
+
+      } else {
+        scope.state.open = false;
+      }
+    };
+
+    scope.handleSearch = () => {
+      scope.state.searching = !scope.state.searching;
+    };
+
+    scope.handleHeader = (posY) => {
+      if (posY > 0) scope.state.scrolling = true;
+      else scope.state.scrolling = false;
+    };
+
+    scope.state = {
+      open: false,
+      searching: false
+    };
+
+    scope.$watch('posY', scope.handleHeader);
   }
 }
