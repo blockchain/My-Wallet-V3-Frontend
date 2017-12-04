b04f5b2392c9480ef4ec3df06d81ccc3a1b15515
plondon <philip@blockchain.com>
2017-12-04 12:27:03 -0500

    feat(BCH): request currency copy

--- assets/js/services/asset-context.service.js
+++ assets/js/services/asset-context.service.js
@@ -2,7 +2,7 @@ angular
   .module('walletApp')
   .factory('assetContext', AssetContextService);
 
-function AssetContextService ($state, Wallet, Ethereum) {
+function AssetContextService ($state, Wallet, Ethereum, BitcoinCash) {
   const service = {};
 
   service.getAssets = () => [
@@ -13,6 +13,7 @@ function AssetContextService ($state, Wallet, Ethereum) {
 
   service.hasBtcBalance = () => Wallet.total() > 0;
   service.hasEthBalance = () => Ethereum.balance > 0;
+  service.hasBchBalance = () => BitcoinCash.balance > 0;
 
   service.activeAsset = () => $state.current.name.includes('btc') && 'btc' ||
                               $state.current.name.includes('bch') && 'bch' ||
