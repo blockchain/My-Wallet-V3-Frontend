790da8ad17cae036a6b4656ca09c68044071870a
plondon <philip@blockchain.com>
2017-12-04 12:15:53 -0500

    feat(BCH): when another currency is present show get started on empty currency

--- assets/js/controllers/home.controller.js
+++ assets/js/controllers/home.controller.js
@@ -2,7 +2,7 @@ angular
   .module('walletApp')
   .controller('HomeCtrl', HomeCtrl);
 
-function HomeCtrl ($scope, MyWallet, Wallet, Ethereum, BitcoinCash, tradeStatus, localStorageService, currency, modals) {
+function HomeCtrl ($scope, MyWallet, Wallet, Ethereum, BitcoinCash, Env, tradeStatus, localStorageService, currency, modals) {
   $scope.btc = {
     total: () => Wallet.total('') + Wallet.total('imported'),
     accounts: MyWallet.wallet.hdwallet.accounts
@@ -15,6 +15,12 @@ function HomeCtrl ($scope, MyWallet, Wallet, Ethereum, BitcoinCash, tradeStatus,
   $scope.bch = {
     total: () => BitcoinCash.balance,
     accounts: MyWallet.wallet.bch.accounts
+  };
+
+  $scope.hasBalance = (currencies) => {
+    let total = 0;
+    currencies.forEach((currency) => total += parseFloat($scope[currency].total()));
+    return total > 0;
   };
 
   $scope.isWalletInitialized = () => {
@@ -42,6 +48,12 @@ function HomeCtrl ($scope, MyWallet, Wallet, Ethereum, BitcoinCash, tradeStatus,
       return true;
     }
   };
+
+  Env.then((env) => {
+    let accountInfo = MyWallet.wallet.accountInfo;
+    let sfox = env.partners.sfox.countries.indexOf(accountInfo.countryCodeGuess) > -1 && env.partners.sfox.states.indexOf(accountInfo.stateCodeGuess) > -1;
+    $scope.canBuy = tradeStatus.canTrade() && !sfox;
+  });
 
   $scope.openRequest = modals.openRequest;
 }
