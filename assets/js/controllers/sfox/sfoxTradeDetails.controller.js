dc3c8cb7f5a84e81b08366851b6ee2d952eead17
Philip Welber <welber@blockchain.com>
2018-02-08 10:10:47 -0500

    feat(SFOX): get buy tx fee from wallet options

--- assets/js/controllers/sfox/sfoxTradeDetails.controller.js
+++ assets/js/controllers/sfox/sfoxTradeDetails.controller.js
@@ -2,7 +2,7 @@ angular
   .module('walletApp')
   .controller('SfoxTradeDetailsController', SfoxTradeDetailsController);
 
-function SfoxTradeDetailsController ($scope, MyWallet, Exchange, currency, sfox) {
+function SfoxTradeDetailsController ($scope, MyWallet, Exchange, currency, sfox, $q) {
   let trade = $scope.$parent.trade || $scope.checkout.trade || $scope.trade;
   let format = currency.formatCurrencyForView;
   let fiat = currency.currencies.find((c) => c.code === 'USD');
@@ -16,7 +16,14 @@ function SfoxTradeDetailsController ($scope, MyWallet, Exchange, currency, sfox)
   $scope.namespace = 'SFOX';
   $scope.state = '.' + trade.state;
   $scope.type = trade.isBuy ? '.buy' : '.sell';
-  $scope.tradeDetails = $scope.type === '.buy' ? sfox.buyTradeDetails(null, trade, tx) : sfox.sellTradeDetails(null, null, trade, tx);
+
+  if ($scope.type === '.buy') {
+    $q.resolve(sfox.buyTradeDetails(null, trade, tx))
+      .then(details => $scope.tradeDetails = details);
+  } else {
+    $scope.tradeDetails = sfox.sellTradeDetails(null, null, trade, tx);
+  }
+
   $scope.rate = $scope.type === '.buy'
     ? format((trade.sendAmount / 1e8) / trade.receiveAmount, fiat, true)
     : format(1 / (trade.sendAmount / 1e8) * (trade.receiveAmount + trade.feeAmount), fiat, true);
